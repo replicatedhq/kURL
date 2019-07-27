5b27f91cebcc73b2b65f39888d5774fe94bbc9e1
Andrew Reed <andrew@replicated.com>
2019-07-26 18:34:01 -0700

    contour

--- scripts/common/contour.sh
+++ scripts/common/contour.sh
@@ -1,4 +1,12 @@
 
 function contour() {
-    return 0
+    render_yaml contour-common.yaml > /tmp/contour-common.yaml
+    render_yaml contour.yaml > /tmp/contour.yaml
+    render_yaml contour-rbac.yaml > /tmp/contour-rbac.yaml
+    render_yaml contour-service.yaml > /tmp/contour-service.yaml
+
+    kubectl apply -f /tmp/contour-common.yaml
+    kubectl apply -f /tmp/contour.yaml
+    kubectl apply -f /tmp/contour-rbac.yaml
+    kubectl apply -f /tmp/contour-service.yaml
 }
