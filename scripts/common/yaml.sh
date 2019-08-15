a4be7917a8df4bf41d6534b693c097b788313fbd
Andrew Reed <andrew@replicated.com>
2019-08-15 09:12:34 -0700

    KURL_URL

--- scripts/common/yaml.sh
+++ scripts/common/yaml.sh
@@ -1,8 +1,8 @@
 
 function render_yaml() {
-    if [ "$AIRGAP" != "1" ] && [ -n "$INSTALL_URL" ]; then
+    if [ "$AIRGAP" != "1" ] && [ -n "$KURL_URL" ]; then
         mkdir -p $YAML_DIR
-        curl $INSTALL_URL/dist/yaml/$1 > $YAML_DIR/$1
+        curl $KURL_URL/dist/yaml/$1 > $YAML_DIR/$1
     fi
 	eval "echo \"$(cat $YAML_DIR/$1)\""
 }
