6fedb527dc97b8c137235edc317a9a00507f3146
Andrew Reed <areed@users.noreply.github.com>
2019-08-22 10:01:16 -0700

    CI parallel jobs and bug fixes (#11)
    * build web for docker image
    
    * circle config export S3_BUCKET
    
    * parallel package upload
    
    * deploy step is non-parallel
    
    * remove manual list of packages
    
    * remove branch from filter
    
    * remove unused test job

--- bin/upload-dist.sh
+++ bin/upload-dist.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Generate the list of all packages to be uploaded, then filter that based on CircleCI parallelism
+# environment variables.
+
+set -euo pipefail
+
 function require() {
     if [ -z "$2" ]; then
         echo "validation failed: $1 unset"
@@ -9,5 +14,35 @@ function require() {
 
 require AWS_ACCESS_KEY_ID "${AWS_ACCESS_KEY_ID}"
 require AWS_SECRET_ACCESS_KEY "${AWS_SECRET_ACCESS_KEY}"
+require CIRCLE_NODE_TOTAL "${CIRCLE_NODE_TOTAL}"
+require CIRCLE_NODE_INDEX "${CIRCLE_NODE_INDEX}"
+require S3_BUCKET "${S3_BUCKET}"
 
-aws s3 cp dist/ s3://$1/dist --recursive
+function pkgs() {
+    for dir in $(find $1 -mindepth 2 -maxdepth 2 -type d)
+    do
+        local name=$(echo $dir | awk -F "/" '{print $2 }')
+        local version=$(echo $dir | awk -F "/" '{print $3 }')
+        echo "${name}-${version}.tar.gz"
+    done
+}
+
+function docker_pkg() {
+    local version=$(grep DOCKER_VERSION scripts/Manifest | awk -F "=" '{ print $2 }')
+    echo "docker-${version}.tar.gz"
+}
+
+function list_all_packages() {
+    pkgs addons
+    pkgs packages
+    echo "common.tar.gz"
+    docker_pkg
+}
+
+for package in $(list_all_packages | awk "NR % $CIRCLE_NODE_TOTAL == $CIRCLE_NODE_INDEX")
+do
+    echo "Making $package"
+    make dist/$package
+done
+
+aws s3 cp dist/ s3://$S3_BUCKET/dist --recursive
