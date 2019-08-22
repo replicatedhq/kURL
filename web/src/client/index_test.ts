0fd752777c3f6e52097cfb9351cb0c38d1ae802f
Andrew Reed <andrew@replicated.com>
2019-08-22 10:57:02 -0700

    restore lost code review changes

--- web/src/client/index_test.ts
+++ web/src/client/index_test.ts
@@ -331,7 +331,7 @@ spec:
     it("returns yaml with version", async () => {
       const yaml = await client.getInstallerYAML("latest", true);
 
-      expect(yaml).to.match(/version: "latest"/);
+      expect(yaml).not.to.match(/version: "latest"/);
     });
   });
 
