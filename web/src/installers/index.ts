78a025368b09f487d9c5fcb6d48bfb3b7a36758c
Marc Campbell <marc.e.campbell@gmail.com>
2019-10-24 00:44:12 +0000

    Past and next kotsadm

--- web/src/installers/index.ts
+++ web/src/installers/index.ts
@@ -379,7 +379,7 @@ export class Installer {
       "0.33.0",
     ],
     kotsadm: [
-      "0.9.9",
+      "0.9.11",
     ],
   }
 
@@ -477,7 +477,7 @@ export class Installer {
   }
 
   static isSHA(id: string): boolean {
-    return /^[0-9a-f]{7}$/.test(id); 
+    return /^[0-9a-f]{7}$/.test(id);
   }
 
   static isValidSlug(id: string): boolean {
