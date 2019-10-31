8cb0e22973c6a6510f53666501123837d033f7ba
Andrew Reed <andrew@replicated.com>
2019-10-31 20:39:55 +0000

    bootstrap token expiration

--- cmd/join/main.go
+++ cmd/join/main.go
@@ -16,12 +16,16 @@ import (
 
 const configMapName = "kurl-config"
 const configMapNamespace = "kube-system"
+
 const bootstrapTokenKey = "bootstrap_token"
+const bootstrapTokenExpirationKey = "bootstrap_token_expiration"
 
 func main() {
 	client := clientsetOrDie()
 
-	ti, err := GenerateBootstrapToken(client, nil)
+	bootstrapTokenDuration := time.Hour * 24
+	bootstrapTokenExpiration := time.Now().Add(bootstrapTokenDuration)
+	bootstrapToken, err := GenerateBootstrapToken(client, bootstrapTokenDuration)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -34,7 +38,8 @@ func main() {
 		log.Panic(err)
 	}
 
-	cm.Data[bootstrapTokenKey] = ti.Token
+	cm.Data[bootstrapTokenKey] = bootstrapToken
+	cm.Data[bootstrapTokenExpirationKey] = bootstrapTokenExpiration.Format(time.RFC3339)
 
 	_, err = client.CoreV1().ConfigMaps(configMapNamespace).Update(cm)
 	if err != nil {
@@ -42,30 +47,20 @@ func main() {
 	}
 }
 
-type TokenInfo struct {
-	Token    string
-	CAHashes []string
-}
-
 // GenerateBootstrapToken will generate a node join token for kubeadm.
-// ttl defines the time to live for this token. Defaults to 24h.
-func GenerateBootstrapToken(client kubernetes.Interface, ttl *time.Duration) (*TokenInfo, error) {
-	var tokenInfo TokenInfo
-
+// ttl defines the time to live for this token.
+func GenerateBootstrapToken(client kubernetes.Interface, ttl time.Duration) (string, error) {
 	token, err := bootstraputil.GenerateBootstrapToken()
 	if err != nil {
-		return nil, errors.Wrap(err, "generate kubeadm token")
+		return "", errors.Wrap(err, "generate kubeadm token")
 	}
 
 	bts, err := kubeadm.NewBootstrapTokenString(token)
 	if err != nil {
-		return nil, errors.Wrap(err, "new kubeadm token string")
+		return "", errors.Wrap(err, "new kubeadm token string")
 	}
 
-	var duration *metav1.Duration
-	if ttl != nil {
-		duration = &metav1.Duration{Duration: *ttl}
-	}
+	duration := &metav1.Duration{Duration: ttl}
 
 	// TODO rbac - Update, Create Secrets in kube-system namespace
 	if err := tokenphase.UpdateOrCreateTokens(client, false, []kubeadm.BootstrapToken{
@@ -76,29 +71,10 @@ func GenerateBootstrapToken(client kubernetes.Interface, ttl *time.Duration) (*T
 			Groups: []string{kubeadmconstants.NodeBootstrapTokenAuthGroup},
 		},
 	}); err != nil {
-		return nil, errors.Wrap(err, "create kubeadm token")
+		return "", errors.Wrap(err, "create kubeadm token")
 	}
 
-	tokenInfo.Token = token
-
-	// kurl-config should have the ca cert hash already
-	// this function could just return a string
-
-	/*
-		rootCAFile := "/var/run/secrets/kubernetes.io/serviceaccount/" + v1.ServiceAccountRootCAKey
-		caCerts, err := clientcertutil.CertsFromFile(rootCAFile)
-		if err != nil {
-			return nil, errors.Wrapf(err, "get certs from file %s", rootCAFile)
-		}
-
-		// hash all the CA certs and include their public key pins as trusted values
-		tokenInfo.CAHashes = make([]string, 0, len(caCerts))
-		for _, caCert := range caCerts {
-			tokenInfo.CAHashes = append(tokenInfo.CAHashes, pubkeypin.Hash(caCert))
-		}
-
-	*/
-	return &tokenInfo, nil
+	return token, nil
 }
 
 func clientsetOrDie() kubernetes.Interface {
