f5fe12024136ab61bf1ba310c4bb2fe7dc21f1ca
Michael Zaneri <michael@Michaels-MacBook-Pro.local>
2019-12-06 11:51:27 -0800

    REFACTOR remove busybox pof from install script and remove filters for rook and kube-system from fluentd conf

--- addons/fluentd/1.3/install.sh
+++ addons/fluentd/1.3/install.sh
@@ -1,5 +1,5 @@
 fluentd () {
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/example_app.yaml
+    # kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/example_app.yaml
     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd.yaml
     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/rbac.yaml
 }
