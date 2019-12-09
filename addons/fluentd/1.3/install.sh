cecebac79da0b93a7d8a6e21e516f22e486b9c7d
Michael Zaneri <michael@Michaels-MacBook-Pro.local>
2019-12-09 12:34:21 -0800

    REFACTOR changing names

--- addons/fluentd/1.3/install.sh
+++ addons/fluentd/1.3/install.sh
@@ -1,5 +1,5 @@
 fluentd () {
-    # kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/example_app.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-ns.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-rbac.yaml
     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd.yaml
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/rbac.yaml
 }
