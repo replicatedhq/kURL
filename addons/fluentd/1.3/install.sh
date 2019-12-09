7d7af4134ab7996fa2bbc9c86e0ec04730bbecab
Michael Zaneri <michael@replicated.com>
2019-12-09 15:04:07 -0800

    FIX rbac stuff for fluentd

--- addons/fluentd/1.3/install.sh
+++ addons/fluentd/1.3/install.sh
@@ -1,5 +1,7 @@
 fluentd () {
     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-ns.yaml
     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-rbac.yaml
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-configmap.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-daemonset.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/test-log-generator.yaml
 }
