4f9f27f9de5d855fa7bef5491785d20fd29b4785
Michael Zaneri <michael@replicated.com>
2019-12-11 18:52:26 -0800

    minimal working setup for fluentd elasticsearch kibana stack

--- addons/fluentd/1.3/install.sh
+++ addons/fluentd/1.3/install.sh
@@ -1,7 +1,19 @@
+# fluentd () {
+#     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_only/fluentd-ns.yaml
+#     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_only/fluentd-rbac.yaml
+#     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_only/fluentd-configmap.yaml
+#     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_only/fluentd-daemonset.yaml
+#     kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/test-log-generator.yaml
+# }
+
 fluentd () {
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-ns.yaml
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-rbac.yaml
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-configmap.yaml
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd-daemonset.yaml
-    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/test-log-generator.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/logging-ns.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/elasticsearch-svc.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/elasticsearch-statefulset.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/kibana.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/kibana-svc.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/fluentd-configmap.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/fluentd-rbac.yaml
+    kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/fluentd_es_kibana/fluentd-daemonset.yaml
+    #kubectl apply -f $DIR/addons/fluentd/$FLUENTD_VERSION/test-log-generator.yaml
 }
