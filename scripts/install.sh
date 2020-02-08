74a3a2e2ddc8e1cd4829e9347a6b1204e7c5c656
mzaneri <michaelzaneri@gmail.com>
2020-02-07 17:40:00 -0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -230,7 +230,6 @@ function main() {
     tasks
     preflights
     prompts
-    setup_installer_crd
     configure_proxy
     install_docker
     get_shared
@@ -251,6 +250,7 @@ function main() {
     addon_pre_init fluentd "$FLUENTD_VERSION"
     addon_pre_init kotsadm "$KOTSADM_VERSION"
     init
+    setup_installer_crd
     addon aws "$AWS_VERSION"
     addon nodeless "$NODELESS_VERSION"
     addon calico "$CALICO_VERSION"
