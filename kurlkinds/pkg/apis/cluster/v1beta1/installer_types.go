1cace9f24bc3af9613a4306d85e9418d9a47c0b0
Michael <michael@replicated.com>
2020-02-11 21:08:20 +0000

    REFACTOR go fmt

--- kurlkinds/pkg/apis/cluster/v1beta1/installer_types.go
+++ kurlkinds/pkg/apis/cluster/v1beta1/installer_types.go
@@ -35,70 +35,70 @@ type InstallerSpec struct {
 	Fluentd    Fluentd    `json:"fluentd,omitempty"`
 	Kotsadm    Kotsadm    `json:"kotsadm,omitempty"`
 	Velero     Velero     `json:"velero,omitempty"`
-	Flags     Flags     `json:"flags,omitempty"`
+	Flags      Flags      `json:"flags,omitempty"`
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
 }
 
 type Flags struct {
-	IsKurl              bool       `json:"isKurl,omitempty"`
+	IsKurl bool `json:"isKurl,omitempty"`
 
-	Airgap         bool       `json:"airgap,omitempty"`
-	BypassStorageDriverWarnings         bool       `json:"bypassStorageDriverWarnings,omitempty"`
-	BootstrapToken         string       `json:"bootstrapToken,omitempty"`
-	BootstrapTokenTTL         string       `json:"bootstrapTokenTTL,omitempty"`
-	CephPoolReplicas         int       `json:"cephPoolReplicas,omitempty"`
-	HostnameCheck         string       `json:"hostnameCheck,omitempty"`
-	HACluster            bool       `json:"HACluster,omitempty"`
-	HTTPProxy         string       `json:"HTTPProxy,omitempty"`
-	IPAllocRange         string       `json:"IPAllocRange,omitempty"`
-	LoadBalancerAddress string     `json:"loadBalancerAddress,omitempty"`
-	LogLevel         string       `json:"logLevel,omitempty"`
-	NoDocker              bool       `json:"noDocker,omitempty"`
-	NoProxy              bool       `json:"noProxy,omitempty"`
-	PublicAddress         string       `json:"publicAddress,omitempty"`
-	PrivateAddress         string       `json:"privateAddress,omitempty"`
-	SkipPull              bool       `json:"skipPull,omitempty"`
-	KubernetesNamespace         string       `json:"kubernetesNamespace,omitempty"`
-	StorageClass         string       `json:"storageClass,omitempty"`
-	NoCEOnEE                   bool   `json:"noCEOnEE,omitempty"`
-	HardFailOnLoopback                   bool   `json:"hardFailOnLoopback,omitempty"`
-	HardFailOnFirewalld                   bool   `json:"hardFailOnFirewalld,omitempty"`
-	BypassFirewalldWarning                   bool   `json:"bypassFirewalldWarning,omitempty"`
-	DisableContour         bool       `json:"disableContour,omitempty"`
-	DisableRook         bool       `json:"disableRook,omitempty"`
-	DisablePrometheus         bool       `json:"disablePrometheus,omitempty"`
-	FullEFKStack         bool       `json:"fullEFKStack,omitempty"`
-	ForceReset         bool       `json:"forceReset,omitempty"`
-	ServiceCIDR         string       `json:"serviceCIDR,omitempty"`
-	ClusterDNS         string       `json:"clusterDNS,omitempty"`
-	EncryptNetwork         string       `json:"encryptNetwork,omitempty"`
-	AdditionalNoProxy             string       `json:"additionalNoProxy,omitempty"`
-	KubernetesUpgradePatchVersion              bool       `json:"kubernetesUpgradePatchVersion,omitempty"`
-	KubernetesMasterAddress         string       `json:"kubernetesMasterAddress,omitempty"`
-	ApiServiceAddress         string       `json:"apiServiceAddress,omitempty"`
-	Insecure         bool       `json:"insecure,omitempty"`
-	KubeadmTokenCAHash         string       `json:"kubeadmTokenCAHash,omitempty"`
-	KubernetesVersion         string       `json:"kubernetesVersion,omitempty"`
-	ControlPlane         bool       `json:"controlPlane,omitempty"`
-	CertKey         string       `json:"certKey,omitempty"`
-	Task         string       `json:"task,omitempty"`
-	DockerRegistryIP         string       `json:"dockerRegistryIP,omitempty"`
-	KotsadmHostname         string       `json:"kotsadmHostname,omitempty"`
-	KotsadmUIBindPort         string       `json:"kotsadmUIBindPort,omitempty"`
-	PodCIDR         string       `json:"podCIDR,omitempty"`
-	RegistryPublishPort         string       `json:"registryPublishPort,omitempty"`
-	KotsadmApplicationNamepsaces         string       `json:"kotsadmApplicationNamepsaces,omitempty"`
-	VeleroNamespace         string       `json:"veleroNamespace,omitempty"`
-	VeleroLocalBucket         string       `json:"veleroLocalBucket,omitempty"`
-	VeleroDisableCLI         bool       `json:"veleroDisableCLI,omitempty"`
-	VeleroDisableRestic         bool       `json:"veleroDisableRestic,omitempty"`
-	KotsadmAlpha         string       `json:"kotsadmAlpha,omitempty"`
-	MinioNamespace         string       `json:"minioNamespace,omitempty"`
-	OpenEBSNamespace         string       `json:"openEBSNamespace,omitempty"`
-	OpenEBSLocalPV         string       `json:"openEBSLocalPV,omitempty"`
-	OpenEBSLocalPVStorageClass         string       `json:"openEBSLocalPVStorageClass,omitempty"`
+	Airgap                        bool   `json:"airgap,omitempty"`
+	BypassStorageDriverWarnings   bool   `json:"bypassStorageDriverWarnings,omitempty"`
+	BootstrapToken                string `json:"bootstrapToken,omitempty"`
+	BootstrapTokenTTL             string `json:"bootstrapTokenTTL,omitempty"`
+	CephPoolReplicas              int    `json:"cephPoolReplicas,omitempty"`
+	HostnameCheck                 string `json:"hostnameCheck,omitempty"`
+	HACluster                     bool   `json:"HACluster,omitempty"`
+	HTTPProxy                     string `json:"HTTPProxy,omitempty"`
+	IPAllocRange                  string `json:"IPAllocRange,omitempty"`
+	LoadBalancerAddress           string `json:"loadBalancerAddress,omitempty"`
+	LogLevel                      string `json:"logLevel,omitempty"`
+	NoDocker                      bool   `json:"noDocker,omitempty"`
+	NoProxy                       bool   `json:"noProxy,omitempty"`
+	PublicAddress                 string `json:"publicAddress,omitempty"`
+	PrivateAddress                string `json:"privateAddress,omitempty"`
+	SkipPull                      bool   `json:"skipPull,omitempty"`
+	KubernetesNamespace           string `json:"kubernetesNamespace,omitempty"`
+	StorageClass                  string `json:"storageClass,omitempty"`
+	NoCEOnEE                      bool   `json:"noCEOnEE,omitempty"`
+	HardFailOnLoopback            bool   `json:"hardFailOnLoopback,omitempty"`
+	HardFailOnFirewalld           bool   `json:"hardFailOnFirewalld,omitempty"`
+	BypassFirewalldWarning        bool   `json:"bypassFirewalldWarning,omitempty"`
+	DisableContour                bool   `json:"disableContour,omitempty"`
+	DisableRook                   bool   `json:"disableRook,omitempty"`
+	DisablePrometheus             bool   `json:"disablePrometheus,omitempty"`
+	FullEFKStack                  bool   `json:"fullEFKStack,omitempty"`
+	ForceReset                    bool   `json:"forceReset,omitempty"`
+	ServiceCIDR                   string `json:"serviceCIDR,omitempty"`
+	ClusterDNS                    string `json:"clusterDNS,omitempty"`
+	EncryptNetwork                string `json:"encryptNetwork,omitempty"`
+	AdditionalNoProxy             string `json:"additionalNoProxy,omitempty"`
+	KubernetesUpgradePatchVersion bool   `json:"kubernetesUpgradePatchVersion,omitempty"`
+	KubernetesMasterAddress       string `json:"kubernetesMasterAddress,omitempty"`
+	ApiServiceAddress             string `json:"apiServiceAddress,omitempty"`
+	Insecure                      bool   `json:"insecure,omitempty"`
+	KubeadmTokenCAHash            string `json:"kubeadmTokenCAHash,omitempty"`
+	KubernetesVersion             string `json:"kubernetesVersion,omitempty"`
+	ControlPlane                  bool   `json:"controlPlane,omitempty"`
+	CertKey                       string `json:"certKey,omitempty"`
+	Task                          string `json:"task,omitempty"`
+	DockerRegistryIP              string `json:"dockerRegistryIP,omitempty"`
+	KotsadmHostname               string `json:"kotsadmHostname,omitempty"`
+	KotsadmUIBindPort             string `json:"kotsadmUIBindPort,omitempty"`
+	PodCIDR                       string `json:"podCIDR,omitempty"`
+	RegistryPublishPort           string `json:"registryPublishPort,omitempty"`
+	KotsadmApplicationNamepsaces  string `json:"kotsadmApplicationNamepsaces,omitempty"`
+	VeleroNamespace               string `json:"veleroNamespace,omitempty"`
+	VeleroLocalBucket             string `json:"veleroLocalBucket,omitempty"`
+	VeleroDisableCLI              bool   `json:"veleroDisableCLI,omitempty"`
+	VeleroDisableRestic           bool   `json:"veleroDisableRestic,omitempty"`
+	KotsadmAlpha                  string `json:"kotsadmAlpha,omitempty"`
+	MinioNamespace                string `json:"minioNamespace,omitempty"`
+	OpenEBSNamespace              string `json:"openEBSNamespace,omitempty"`
+	OpenEBSLocalPV                string `json:"openEBSLocalPV,omitempty"`
+	OpenEBSLocalPVStorageClass    string `json:"openEBSLocalPVStorageClass,omitempty"`
 }
 
 type Kubernetes struct {
