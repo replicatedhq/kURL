2689de51a080b58a364afe92080c6a6ae64d1efb
Andrew Lavery <laverya@umich.edu>
2020-02-13 11:27:25 -0800

    remove 'cp kustomization.yaml' step, as that file is generated

--- addons/contour/1.0.1/install.sh
+++ addons/contour/1.0.1/install.sh
@@ -9,11 +9,10 @@ function contour() {
     local src="$DIR/addons/contour/1.0.1"
     local dst="$DIR/kustomize/contour"
 
-    cp "$src/kustomization.yaml" "$dst/"
     cp "$src/common.yaml" "$dst/"
     cp "$src/deployment.yaml" "$dst/"
-    cp "$src/service.yaml" "$dst/"
     cp "$src/rbac.yaml" "$dst/"
+    cp "$src/service.yaml" "$dst/"
 
     cp "$src/patches/service-patch.yaml" "$dst/"
 
