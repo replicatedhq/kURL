902faabd3ef8458715d9fc938c362e6e1b5bd3f8
Michael <michael@replicated.com>
2020-02-14 17:34:36 +0000

    ADD created test for CRD

---
+++ kurlkinds/config/test/test_installer_crd.sh
@@ -0,0 +1,15 @@
+DOCKER_VERSION=poop
+NO_PROXY=1
+KUBERNETES_VERSION="latest"
+WEAVE_VERSION="latest"
+MINIO_VERSION="latest"
+OPENEBS_VERSION="latest"
+PROMETHEUS_VERSION="latest"
+FLUENTD_VERSION="latest"
+ROOK_VERSION="latest"
+REGISTRY_VERSION="latest"
+VELERO_VERSION="latest"
+CONTOUR_VERSION="latest"
+KOTSADM_VERSION="latest"
+
+apply_flags_to_yaml "$1"
