e387da8320d886fcc24ad02fbf5e39c3beef6042
Ethan Mosbaugh <ethan@replicated.com>
2020-02-28 02:23:24 +0000

    Ekco purge node command

---
+++ addons/ekco/0.2.0/ekco-purge-node.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]; then
+    printf "[usage] ekco-purge-node [name]\n" 1>&2
+    exit 1
+fi
+
+set -u
+
+export KUBECONFIG="${KUBECONFIG:-/etc/kubernetes/admin.conf}"
+
+namespace=kurl
+pod="$(kubectl get pods -n "$namespace" -l app=ekc-operator --field-selector=status.phase=Running --no-headers 2>/dev/null | awk 'NR == 1 {print $1}')"
+
+if [ -z "$pod" ]; then
+    printf "[error] no running EKCO pods found\n\n" 1>&2
+    (set -x; kubectl get pods -n "$namespace" -l app=ekc-operator 1>&2)
+    exit 1
+fi
+
+kubectl exec -n "$namespace" -it "$pod" ekco purge-node "$1"
