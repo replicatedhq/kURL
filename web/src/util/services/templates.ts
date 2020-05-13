a9cfdd5c4c84d227ea20b5d0ec41049e600351b4
Andrew Reed <areed@users.noreply.github.com>
2020-05-12 18:07:09 -0700

    remove unused bash variables (#455)
    Remove unused bash variables

--- web/src/util/services/templates.ts
+++ web/src/util/services/templates.ts
@@ -61,24 +61,9 @@ interface Manifest {
   KURL_URL: string;
   DIST_URL: string;
   INSTALLER_ID: string;
-  KUBERNETES_VERSION: string;
-  WEAVE_VERSION: string;
-  ROOK_VERSION: string;
-  OPENEBS_VERSION: string;
-  MINIO_VERSION: string;
-  CONTOUR_VERSION: string;
-  REGISTRY_VERSION: string;
-  PROMETHEUS_VERSION: string;
-  FLUENTD_VERSION: string;
-  KOTSADM_VERSION: string;
-  KOTSADM_APPLICATION_SLUG: string;
   REPLICATED_APP_URL: string;
-  VELERO_VERSION: string;
-  EKCO_VERSION: string;
-  FLAGS: string;
   KURL_UTIL_IMAGE: string;
   KURL_BIN_UTILS_FILE: string;
-  DOCKER_VERSION: string;
   INSTALLER_YAML: string;
 }
 
@@ -87,22 +72,7 @@ function manifestFromInstaller(i: Installer, kurlURL: string, replicatedAppURL: 
     KURL_URL: kurlURL,
     DIST_URL: distURL,
     INSTALLER_ID: i.id,
-    KUBERNETES_VERSION: i.spec.kubernetes.version,
-    WEAVE_VERSION: _.get(i.spec, "weave.version", ""),
-    ROOK_VERSION: _.get(i.spec, "rook.version", ""),
-    OPENEBS_VERSION: _.get(i.spec, "openebs.version", ""),
-    MINIO_VERSION: _.get(i.spec, "minio.version", ""),
-    CONTOUR_VERSION: _.get(i.spec, "contour.version", ""),
-    REGISTRY_VERSION: _.get(i.spec, "registry.version", ""),
-    PROMETHEUS_VERSION: _.get(i.spec, "prometheus.version", ""),
-    FLUENTD_VERSION: _.get(i.spec, "fluentd.version", ""),
-    KOTSADM_VERSION: _.get(i.spec, "kotsadm.version", ""),
-    KOTSADM_APPLICATION_SLUG: _.get(i.spec, "kotsadm.applicationSlug", ""),
     REPLICATED_APP_URL: replicatedAppURL,
-    VELERO_VERSION: _.get(i.spec, "velero.version", ""),
-    EKCO_VERSION: _.get(i.spec, "ekco.version", ""),
-    DOCKER_VERSION: _.get(i.spec, "docker.version", ""),
-    FLAGS: i.flags(),
     KURL_UTIL_IMAGE: kurlUtilImage,
     KURL_BIN_UTILS_FILE: kurlBinUtils,
     INSTALLER_YAML: i.toYAML(),
