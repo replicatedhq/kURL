6ac5e789d1ee306dc58ecf7d744ed30417b98468
Andrew Reed <andrew@replicated.com>
2020-05-14 17:05:54 +0000

    bootstrap proxy

--- scripts/common/proxy.sh
+++ scripts/common/proxy.sh
@@ -1,4 +1,28 @@
 
+# Need to peek at the yaml spec with bash to find if a proxy is needed to download the util binaries
+# TODO QA proxy from installer yaml
+# TODO QA from override spec file
+# TODO QA set in both yaml and local spec file
+function proxy_bootstrap() {
+    if [ "$NO_PROXY" = "1" ]; then
+        return
+    fi
+    if [ -n "$INSTALLER_SPEC_FILE" ]; then
+        local overrideProxy=$(grep "proxyAddress:" "$INSTALLER_SPEC_FILE" | grep -o "http[^'\" ]*")
+        if [ -n "$overrideProxy" ]; then
+            export https_proxy="$overrideProxy"
+            echo "TODO REMOVE. Proxy address from installer spec file: $https_proxy"
+            return
+        fi
+    fi
+    local proxy=$(cat "$INSTALLER_YAML" | grep -o "http[^'\" ]*")
+    if [ -n "$proxy" ]; then
+        export https_proxy="$proxy"
+        echo "TODO REMOVE. Proxy address from installer yaml: $https_proxy"
+        return
+    fi
+}
+
 function configure_proxy() {
     if [ "$NO_PROXY" = "1" ]; then
         return
