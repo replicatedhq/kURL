d6c26d4a915e84b40015fff19d415c81da678b55
Andrew Reed <andrew@replicated.com>
2020-05-14 01:06:48 +0000

    export

--- scripts/common/proxy.sh
+++ scripts/common/proxy.sh
@@ -1,25 +1,17 @@
 
 function configure_proxy() {
-    exportProxy
+    if [ "$NO_PROXY" = "1" ]; then
+        return
+    fi
+	if [ -z "$PROXY_ADDRESS" ]; then
+		return
+	fi
+
+	# for curl to download packages
+	export http_proxy="$PROXY_ADDRESS"
+
+    curl --silent --fail https://api.replicated.com/market/v1/echo/ip
+
     # kubeadm requires this in the environment to reach the K8s API server
     export no_proxy="$NO_PROXY_ADDRESSES"
 }
-
-exportProxy() {
-    if [ -z "$PROXY_ADDRESS" ]; then
-        return
-    fi
-    if [ -z "$http_proxy" ]; then
-       export http_proxy=$PROXY_ADDRESS
-    fi
-    if [ -z "$https_proxy" ]; then
-       export https_proxy=$PROXY_ADDRESS
-    fi
-    if [ -z "$HTTP_PROXY" ]; then
-       export HTTP_PROXY=$PROXY_ADDRESS
-    fi
-    if [ -z "$HTTPS_PROXY" ]; then
-       export HTTPS_PROXY=$PROXY_ADDRESS
-    fi
-}
-
