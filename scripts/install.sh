4dcd05127646a8057e8a46ad23885be636676c34
mzaneri <michaelzaneri@gmail.com>
2020-06-10 17:27:23 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -121,8 +121,6 @@ EOF
         | tee /tmp/kubeadm-init
     set +o pipefail
 
-    confirmY
-
     exportKubeconfig
     KUBEADM_TOKEN_CA_HASH=$(cat /tmp/kubeadm-init | grep 'discovery-token-ca-cert-hash' | awk '{ print $2 }' | head -1)
 
