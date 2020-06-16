7333e6c4e611e2a53ba1975432b4bade1334b18b
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2020-06-15 17:00:47 -0700

    deploying testgrid web (#528)
    * deploying testgrid web

---
+++ testgrid/web/webpack.config.staging.js
@@ -0,0 +1,56 @@
+var path = require("path");
+var webpack = require("webpack");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+var srcPath = path.join(__dirname, "src");
+var distPath = path.join(__dirname, "dist-staging");
+
+module.exports = {
+  entry: [
+    "./src/index.jsx",
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        enforce: "pre",
+        loader: "tslint-loader",
+      },
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        loader: "awesome-typescript-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "disabled",
+      generateStatsFile: true,
+      statsOptions: { source: false }
+    }),
+  ],
+
+  output: {
+    path: distPath,
+    publicPath: "/",
+    filename: "kurltestgrid.[hash].js",
+  },
+
+  devtool: false,
+
+  stats: {
+    colors: true,
+    reasons: false
+  },
+};
