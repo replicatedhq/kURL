1143c06f21b797562ba823dbb97ffc865949aac5
Michael <michael@replicated.com>
2020-06-25 20:03:32 +0000

    FIX remove nodeUnreachableTolerationDuration

--- web/src/test/installers.ts
+++ web/src/test/installers.ts
@@ -72,7 +72,7 @@ spec:
     localBucket: local
   ekco:
     version: latest
-    nodeUnreachableTolerationDuration: 10m
+    nodeUnreachableToleration: 10m
     minReadyMasterNodeCount: 3
     minReadyWorkerNodeCount: 1
     shouldDisableRebootService: false
@@ -248,7 +248,7 @@ const ekco = `
 spec:
   ekco:
     version: latest
-    nodeUnreachableTolerationDuration: 10m
+    nodeUnreachableToleration: 10m
     minReadyMasterNodeCount: 3
     minReadyWorkerNodeCount: 1
     shouldDisableRebootService: false
@@ -658,7 +658,7 @@ spec:
 
       expect(i.spec.ekco).to.deep.equal({
         version: "latest",
-        nodeUnreachableTolerationDuration: "10m",
+        nodeUnreachableToleration: "10m",
         minReadyMasterNodeCount: 3,
         minReadyWorkerNodeCount: 1,
         shouldDisableRebootService: false,
