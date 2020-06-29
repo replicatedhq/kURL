e219c195711c962abd75b6e39e962f2c6160f885
Michael <michael@replicated.com>
2020-06-29 14:30:01 +0000

    ADD ubuntu 16 support

--- testgrid/tgrun/pkg/scheduler/static.go
+++ testgrid/tgrun/pkg/scheduler/static.go
@@ -6,9 +6,21 @@ import (
 
 var operatingSystems = []types.OperatingSystemImage{
 	{
+		VMImageURI: "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2",
+		Name:       "CentOS",
+		Version:    "7",
+		PVCPrefix:  "centos-7-",
+	},
+	{
 		VMImageURI: "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img",
 		Name:       "Ubuntu",
 		Version:    "18.04",
 		PVCPrefix:  "ubuntu-18-",
 	},
+	// {
+	// 	VMImageURI: "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img",
+	// 	Name:       "Ubuntu",
+	// 	Version:    "16.04",
+	// 	PVCPrefix:  "ubuntu-16-",
+	// },
 }
