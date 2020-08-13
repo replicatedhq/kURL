85f041610b2bae342f8feebbbfb64fc8977df97a
Andrew Reed <andrew@replicated.com>
2020-08-13 01:09:22 +0000

    Fix

--- scripts/common/kubernetes.sh
+++ scripts/common/kubernetes.sh
@@ -17,7 +17,7 @@ function kubernetes_host() {
 
     load_images $DIR/packages/kubernetes/$KUBERNETES_VERSION/images
 
-    install_krew
+    install_plugins
 
     install_kustomize
 }
@@ -263,16 +263,16 @@ function kubernetes_secret_value() {
     kubectl -n "$ns" get secret "$name" -ojsonpath="{ .data.$key }" 2>/dev/null | base64 --decode
 }
 
-function install_krew() {
+function install_plugins() {
     pushd "$DIR/krew"
     tar xzvf outdated.tar.gz && mv outdated /usr/local/bin/kubectl-outdated
     tar xzvf preflight.tar.gz && mv preflight /usr/local/bin/kubectl-preflight
-    tar xzvf support-bundle.tar.gz && mv support-bundle /usr/local/bin/kubectl-support-bundle
+    tar xzvf support-bundle.tar.gz && mv support-bundle /usr/local/bin/kubectl-support_bundle
     popd
 
     # uninstall system-wide krew from old versions of kurl
     rm -rf /opt/replicated/krew
-    sed -i '/^export KUBECTL_PLUGINS_PATH.*replicated/d' /etc/profile
+    sed -i '/^export KUBECTL_PLUGINS_PATH.*KREW_ROOT/d' /etc/profile
     sed -i '/^export KREW_ROOT.*replicated/d' /etc/profile
 }
 
