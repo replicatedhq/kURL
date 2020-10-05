7aecf3a219a9dad86fa7d9399a3e50674ad48fe5
Andrew Lavery <laverya@umich.edu>
2020-10-05 15:13:22 -0700

    report errors failing to start testgrid run

--- testgrid/tgrun/pkg/scheduler/static.go
+++ testgrid/tgrun/pkg/scheduler/static.go
@@ -12,9 +12,9 @@ var operatingSystems = []types.OperatingSystemImage{
 		PVCPrefix:  "ubuntu-18-",
 	},
 	{
-	 	VMImageURI: "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img",
-	 	Name:       "Ubuntu",
-	 	Version:    "16.04",
-	 	PVCPrefix:  "ubuntu-16-",
+		VMImageURI: "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img",
+		Name:       "Ubuntu",
+		Version:    "16.04",
+		PVCPrefix:  "ubuntu-16-",
 	},
 }
