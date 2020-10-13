65c008727760234d6100cd23057f9018e4198f80
Ethan Mosbaugh <ethan@replicated.com>
2020-10-13 20:13:20 +0000

    Testgrid: Centos 8.2

--- testgrid/tgrun/pkg/scheduler/static.go
+++ testgrid/tgrun/pkg/scheduler/static.go
@@ -24,6 +24,12 @@ var operatingSystems = []types.OperatingSystemImage{
 		PVCPrefix:  "centos-78-",
 	},
 	{
+		VMImageURI: "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.2.2004-20200611.2.x86_64.qcow2",
+		Name:       "CentOS",
+		Version:    "8.2",
+		PVCPrefix:  "centos-82-",
+	},
+	{
 		VMImageURI: "https://cdn.amazonlinux.com/os-images/2.0.20200917.0/kvm/amzn2-kvm-2.0.20200917.0-x86_64.xfs.gpt.qcow2",
 		Name:       "Amazon Linux",
 		Version:    "2.0",
