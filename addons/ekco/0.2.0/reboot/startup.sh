30b692089d760722ccc06e439664881a340f6135
Ethan Mosbaugh <ethan@replicated.com>
2020-10-15 20:20:53 +0000

    Hostname with server makes kurl install hang

--- addons/ekco/0.2.0/reboot/startup.sh
+++ addons/ekco/0.2.0/reboot/startup.sh
@@ -8,7 +8,7 @@ fi
 export KUBECONFIG=/etc/kubernetes/kubelet.conf
 
 # wait for Kubernets API
-master=$(cat /etc/kubernetes/kubelet.conf | grep server | awk '{ print $2 }')
+master=$(cat /etc/kubernetes/kubelet.conf | grep ' server:' | awk '{ print $2 }')
 while [ "$(curl --noproxy "*" -sk $master/healthz)" != "ok" ]; do
         sleep 1
 done
