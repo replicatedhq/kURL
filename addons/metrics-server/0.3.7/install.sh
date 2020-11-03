3006334f924f803d40cba8d79cf32bd78ca5650f
Andrew Reed <andrew@replicated.com>
2020-11-03 00:28:45 +0000

    kubelet insecure tls patch for metrics server

--- addons/metrics-server/0.3.7/install.sh
+++ addons/metrics-server/0.3.7/install.sh
@@ -3,6 +3,7 @@ metrics-server() {
   local dst="$DIR/kustomize/metrics-server"
 
   cp "$src/components.yaml" "$dst"
+  cp "$src/kubelet-insecure-tls.yaml" "$dst"
   cp "$src/kustomization.yaml" "$dst"
 
   kubectl apply -k  "$dst"
