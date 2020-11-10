d265af07789798825f25b81b18128620d35dedf0
Ethan Mosbaugh <ethan@replicated.com>
2020-11-10 23:08:06 +0000

    oops

--- testgrid/tgrun/pkg/instances/instances.go
+++ testgrid/tgrun/pkg/instances/instances.go
@@ -5,8 +5,5 @@ import "github.com/replicatedhq/kurl/testgrid/tgrun/pkg/scheduler/types"
 var Instances = []types.Instance{}
 
 func RegisterInstance(instance types.Instance) {
-	if instance.InstallerSpec.Docker == nil || instance.InstallerSpec.Docker.Version != "19.03.4" {
-		return
-	}
 	Instances = append(Instances, instance)
 }
