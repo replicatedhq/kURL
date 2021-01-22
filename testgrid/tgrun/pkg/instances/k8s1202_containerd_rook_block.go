db70ebca6bf8eca87aae75a515189de69947d507
Andrew Lavery <laverya@umich.edu>
2021-01-22 12:22:52 -0500

    add to index, add tests

--- testgrid/tgrun/pkg/instances/k8s1202_containerd_rook_block.go
+++ testgrid/tgrun/pkg/instances/k8s1202_containerd_rook_block.go
@@ -10,34 +10,36 @@ func init() {
 		types.Instance{
 			InstallerSpec: types.InstallerSpec{
 				Kubernetes: kurlv1beta1.Kubernetes{
-					Version: "1.19.7",
+					Version: "1.20.2",
 				},
 				Weave: &kurlv1beta1.Weave{
-					Version: "2.6.4",
+					Version: "2.7.0",
+				},
+				Containerd: &kurlv1beta1.Containerd{
+					Version: "1.3.7",
 				},
 				Rook: &kurlv1beta1.Rook{
-					Version: "1.0.4",
-				},
-				Ekco: &kurlv1beta1.Ekco{
-					Version: "0.6.0",
+					Version:               "1.4.3",
+					IsBlockStorageEnabled: true,
 				},
 				Contour: &kurlv1beta1.Contour{
-					Version: "1.0.1",
-				},
-				Docker: &kurlv1beta1.Docker{
-					Version: "19.03.10",
-				},
-				Prometheus: &kurlv1beta1.Prometheus{
-					Version: "0.33.0",
+					Version: "1.7.0",
 				},
 				Registry: &kurlv1beta1.Registry{
 					Version: "2.7.1",
 				},
-				Velero: &kurlv1beta1.Velero{
-					Version: "1.2.0",
+				Ekco: &kurlv1beta1.Ekco{
+					Version: "0.7.0",
+				},
+				Fluentd: &kurlv1beta1.Fluentd{
+					FullEFKStack: true,
+					Version:      "1.7.4",
+				},
+				CertManager: &kurlv1beta1.CertManager{
+					Version: "1.0.3",
 				},
 				Kotsadm: &kurlv1beta1.Kotsadm{
-					Version: "1.29.0",
+					Version: "1.25.2",
 				},
 			},
 		},
