f9106d382e716a79548e7a397116aa3c1da27c95
DanStough <dans@replicated.com>
2021-01-25 22:45:06 +0000

    action fixes

--- bin/test-addon-pr.sh
+++ bin/test-addon-pr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # assumptions
 # - will only run the latest version of an addon if there are multiple version changes (latest is simple dictionary sort :/ )
-# - will run all addon changes in a single spec if multiple addons were changed.
+# - will each addon change as a separate testgrid run. Testing two addon updates simultaneously is not supported.
 
 set -eo pipefail
 
@@ -22,58 +22,107 @@ require AWS_ACCESS_KEY_ID "${AWS_ACCESS_KEY_ID}"
 require AWS_SECRET_ACCESS_KEY "${AWS_SECRET_ACCESS_KEY}"
 require S3_BUCKET "${S3_BUCKET}"
 
+# These addons aren't part of the spec.
+ADDON_DENY_LIST="aws canlico nodeless"
+
 PR_NUMBER=$(echo $GITHUB_REF | cut -d"/" -f3)
 
-INSTALLER_AVAILABLE=
-prepare_addon() {
+# Checks if there is an update to a addon that meets all of the right criteria and reports to the action
+# - Not an update to the template
+# - Not an update to a readme
+# - Not a addon in the ADDON_DENY_LIST (not part of the spec)
+ADDON_AVAILBLE=
+check() {
+  echo "Checking PR#${PR_NUMBER}..."
+
+  # Take the base branch and figure out which addons changed. Verify each.
+  for addon in $(git diff --dirstat=files,0 "origin/${GITHUB_BASE_REF}" -- addons/ "origin/${GITHUB_BASE_REF}" -- addons/ | sed 's/^[ 0-9.]\+% addons\///g' | grep -v template | cut -f -1 -d"/" | uniq ) 
+  do
+    if [ !" $ADDON_DENY_LIST " =~ .*\ $addon\ .*  ]; then
+      check_addon $addon
+    fi
+  done
+
+  if [ -n "${ADDON_AVAILBLE}" ]; then
+    echo "Modified addons detected. Continuing with action..."
+    echo "::set-output name=addons_available::true"    
+  else
+    echo "No changed addons detected, addon is currently in the ADDON_DENY_LIST, or addon does not have a TestGrid template."
+    echo "::set-output name=addons_available::false"
+  fi
+}
+
+
+check_addon() {
   local name=$1
 
-  INSTALLER_AVAILABLE="true"
+  # Get the version that's changed (filter out templates)
+  local version=$(git diff --dirstat=files,0 "origin/${GITHUB_BASE_REF}" -- "addons/${name}" "origin/${GITHUB_BASE_REF}" -- "addons/${name}" | sed 's/^[ 0-9.]\+% addons\///g' | grep -v template | cut -f2 -d"/" | uniq |  sort -r | head -n 1)
+
+  # check if there is a valid version (files in the root don't count)
+  if [ -n "${version}" ]; then
+    ADDON_AVAILBLE=true
+    echo "Found Modified Addon: $name-$version"
+  fi
+}
+
+INSTALLER_AVAILABLE=
+run_addon() {
+  local name=$1
 
   # Get the version that's changed
   local version=$(git diff --dirstat=files,0 "origin/${GITHUB_BASE_REF}" -- "addons/${name}" "origin/${GITHUB_BASE_REF}" -- "addons/${name}" | sed 's/^[ 0-9.]\+% addons\///g' | grep -v template | cut -f2 -d"/" | uniq |  sort -r | head -n 1)
 
-  echo "Found Modified Addon: $name-$version"
+  # check if there is a valid version (files in the root don't count)
+  if [ -n "${version}" ]; then
+    echo "Testing Modified Addon: $name-$version"
 
-  # Concat Spec
-  echo "$(snakecase_to_camelcase $name):" >> ./testgrid/tgrun/hack/installer.yaml
-  echo "  version: ${version}" >> ./testgrid/tgrun/hack/installer.yaml
-  echo "  s3Override: https://${S3_BUCKET}.s3.amazonaws.com/pr/${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}.tar.gz" >> ./testgrid/tgrun/hack/installer.yaml
-
-  # Push to S3
-  echo "Building Package: $name-$version.tag.gz"
-  
-  make "dist/${name}-${version}.tar.gz"
-  aws s3 cp "dist/${name}-${version}.tar.gz" "s3://${S3_BUCKET}/pr/${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}.tar.gz"
-
-  echo "Package pushed to:  s3://${S3_BUCKET}/pr/${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}.tar.gz"
-}
-
-main() {
-  echo "Evaluating PR#${PR_NUMBER}..."
-
-  # Take the base branch and figure out which addons changed. Process Each
-  for addon in $(git diff --dirstat=files,0 "origin/${GITHUB_BASE_REF}" -- addons/ "origin/${GITHUB_BASE_REF}" -- addons/ | sed 's/^[ 0-9.]\+% addons\///g' | grep -v template | cut -f -1 -d"/" | uniq ) 
-  do
-    prepare_addon $addon
-  done
-
-  if [ -n "${INSTALLER_AVAILABLE}" ]; then
-    echo "Installer spec generated."
-
-    MSG="Testgrid Run Executing @ https://testgrid.kurl.sh/run/pr-$(echo $GITHUB_REF | cut -d/ -f3)-${GITHUB_SHA:0:7}"
-    echo "::set-output name=installer_available::true"
-    echo "::set-output name=msg::$MSG"
+    # Build Packages
+    echo "Building Package: $name-$version.tag.gz"
     
-  else
-    echo "::set-output name=installer_available::false"
-    echo "No changed addons detected."
+    make "dist/${name}-${version}.tar.gz"
+    aws s3 cp "dist/${name}-${version}.tar.gz" "s3://${S3_BUCKET}/pr/${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}.tar.gz"
+
+    echo "Package pushed to:  s3://${S3_BUCKET}/pr/${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}.tar.gz"
+
+    # Run for each template (if available)
+    shopt -s nullglob
+    for test_spec in ./addons/$name/template/testgrid/*.yaml;
+    do
+      test_addon $name $version $test_spec
+    done     
   fi
 }
 
-snakecase_to_camelcase() {
-  echo $1 | sed -r 's/-([a-z])/\U\1/gi' | sed -r 's/^([A-Z])/\l\1/'
+test_addon() {
+  local name=$1
+  local version=$2
+  local test_spec=$3
+
+  echo "Found test spec template $test_spec."
+
+  # Substitute
+  local dist="https://${S3_BUCKET}.s3.amazonaws.com/pr/${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}.tar.gz"
+  sed -i "s/__testver__/$version/g" $test_spec
+  sed -i "s/__testdist__/$dist/g" $test_spec
+
+  # Run testgrid plan
+  ./testgrid/tgrun/bin/tgrun queue --staging --ref "pr-${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}" --spec "$(cat $test_spec)"
+  echo "Submitted TestGrid Ref pr-${PR_NUMBER}-${GITHUB_SHA:0:7}-${name}-${version}"
 }
 
-export -f prepare_addon
-main
+run() {
+  echo "Test PR#${PR_NUMBER}..."
+
+  # Take the base branch and figure out which addons changed. Verify each.
+  for addon in $(git diff --dirstat=files,0 "origin/${GITHUB_BASE_REF}" -- addons/ "origin/${GITHUB_BASE_REF}" -- addons/ | sed 's/^[ 0-9.]\+% addons\///g' | grep -v template | cut -f -1 -d"/" | uniq ) 
+  do
+    if [ !" $ADDON_DENY_LIST " =~ .*\ $addon\ .*  ]; then
+      run_addon $addon
+    fi
+  done
+
+  echo "Run completed."
+}
+
+export -f test_addon
