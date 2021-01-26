febf7004a42d684274f4b6742114dfd568ce12b0
emosbaugh <ethan@replicated.com>
2021-01-26 15:18:52 -0800

    Fix bash flag overrides (#1060)
    * Fix bash flag overrides
    
    * no temp var

--- scripts/common/utilbinaries.sh
+++ scripts/common/utilbinaries.sh
@@ -25,7 +25,7 @@ function download_util_binaries() {
 
 function apply_bash_flag_overrides() {
     if [ -n "$1" ]; then
-        $BIN_BASHTOYAML -c $MERGED_YAML_SPEC -f "$@"
+        $BIN_BASHTOYAML -c $MERGED_YAML_SPEC -f "$*"
     fi
 }
 
