adcb02691230caf3042cfb87bc01150efb93c9fd
emosbaugh <ethan@replicated.com>
2021-03-11 15:01:04 -0800

    Host preflight tcp port status (#1309)
    * Host preflight tcp port status
    
    * deps

--- pkg/installer/template.go
+++ pkg/installer/template.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"text/template"
 
+	"github.com/Masterminds/sprig"
 	"github.com/pkg/errors"
 	clusterv1beta1 "github.com/replicatedhq/kurl/kurlkinds/pkg/apis/cluster/v1beta1"
 )
@@ -16,7 +17,7 @@ type TemplateData struct {
 }
 
 func ExecuteTemplate(name, text string, data TemplateData) ([]byte, error) {
-	t, err := template.New(name).Parse(text)
+	t, err := template.New(name).Funcs(sprig.TxtFuncMap()).Parse(text)
 	if err != nil {
 		return nil, errors.Wrap(err, "parse")
 	}
