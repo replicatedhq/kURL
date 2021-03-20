9e27bea884164d904f88279c0b66c1385cabfa26
Andrew Lavery <laverya@umich.edu>
2021-03-19 23:09:03 -0400

    add testgrid with k8s 1.20.3
    also longhorn, latest prometheus/containerd/contour/velero
    
    this is the only test to specifically hit prometheus 0.44.1, containerd 1.4.4, contour 1.13.1, or velero 1.5.3

---
+++ testgrid/tgrun/pkg/instances/k8s120_containerd144.go
@@ -0,0 +1,45 @@
+package instances
+
+import (
+	kurlv1beta1 "github.com/replicatedhq/kurl/kurlkinds/pkg/apis/cluster/v1beta1"
+	"github.com/replicatedhq/kurl/testgrid/tgrun/pkg/scheduler/types"
+)
+
+func init() {
+	RegisterAirgapAndOnlineInstance(
+		types.Instance{
+			InstallerSpec: types.InstallerSpec{
+				Kubernetes: &kurlv1beta1.Kubernetes{
+					Version: "1.20.3",
+				},
+				Weave: &kurlv1beta1.Weave{
+					Version: "2.7.0",
+				},
+				Longhorn: &kurlv1beta1.Longhorn{
+					Version: "1.1.0",
+				},
+				Ekco: &kurlv1beta1.Ekco{
+					Version: "latest",
+				},
+				Contour: &kurlv1beta1.Contour{
+					Version: "1.13.1",
+				},
+				Containerd: &kurlv1beta1.Containerd{
+					Version: "1.4.4",
+				},
+				Prometheus: &kurlv1beta1.Prometheus{
+					Version: "0.44.1",
+				},
+				Registry: &kurlv1beta1.Registry{
+					Version: "2.7.1",
+				},
+				Velero: &kurlv1beta1.Velero{
+					Version: "1.5.3",
+				},
+				Kotsadm: &kurlv1beta1.Kotsadm{
+					Version: "latest",
+				},
+			},
+		},
+	)
+}
