1f7b187bf32291d41ee18620f8ea3791932834bc
Ethan Mosbaugh <ethan@replicated.com>
2021-04-13 23:12:08 +0000

    Rendering manifest installer yaml should not strip quotes when only integers

--- web/src/test/services/templates.ts
+++ web/src/test/services/templates.ts
@@ -1,7 +1,8 @@
 import {describe, it} from "mocha";
 import {expect} from "chai";
-import { bashStringEscape } from "../../util/services/templates";
+import { bashStringEscape, manifestFromInstaller } from "../../util/services/templates";
 import * as _ from "lodash";
+import { Installer } from "../../installers";
 
 
 describe("Escape Bash Special Characters", () => {
@@ -24,4 +25,32 @@ daemonConfig: |
     expect(out).to.equal(singleQuotes);
   });
 
+});
+
+describe("When rendering installer yaml", () => {
+  it("does not strip double quotes from integers", () => {
+    const yaml = `apiVersion: cluster.kurl.sh/v1beta1
+kind: Installer
+metadata:
+  name: "0668700"
+spec:
+  kubernetes:
+    version: 1.19.9
+  docker:
+    version: 20.10.5
+  weave:
+    version: 2.6.5
+  rook:
+    isBlockStorageEnabled: true
+    version: 1.4.3
+  prometheus:
+    version: 0.46.0
+  sonobuoy:
+    version: 0.50.0
+`;
+    const installer = Installer.parse(yaml);
+
+    const manifest = manifestFromInstaller(installer, "KURL_URL", "APP_URL", "DIST_URL", "UTIL_IMAGE", "BINUTILS_IMAGE");
+    expect(manifest.INSTALLER_YAML).to.equal(yaml);
+  });
 });
