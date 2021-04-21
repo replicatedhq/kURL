537ea4ce4386d53c154ec54a0d7eb82bd2a5fa78
Ethan Mosbaugh <ethan@replicated.com>
2021-04-21 18:01:37 +0000

    Copy versioned staging packages to prod

--- bin/upload-dist-staging.sh
+++ bin/upload-dist-staging.sh
@@ -13,11 +13,16 @@ require AWS_ACCESS_KEY_ID "${AWS_ACCESS_KEY_ID}"
 require AWS_SECRET_ACCESS_KEY "${AWS_SECRET_ACCESS_KEY}"
 require S3_BUCKET "${S3_BUCKET}"
 
+GITSHA="$(git rev-parse HEAD)"
+
 function upload() {
     local package="$1"
 
-    make dist/$package
-    aws s3 cp dist/$package s3://$S3_BUCKET/staging/
+    make "dist/${package}"
+    MD5="$(openssl md5 -binary "dist/${package}" | base64)"
+    aws s3 cp "dist/${package}" "s3://${S3_BUCKET}/staging/${GITSHA}/$PKG" \
+        --metadata md5="${MD5}",gitsha="${GITSHA}"
+    aws s3 cp "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" "s3://${S3_BUCKET}/staging/$PKG"
     make clean
     if [ -n "$DOCKER_PRUNE" ]; then
         docker system prune --all --force
@@ -30,9 +35,10 @@ upload kurl-bin-utils-latest.tar.gz
 
 for package in $(bin/list-all-packages.sh)
 do
-    if ! aws s3api head-object --bucket=$S3_BUCKET --key=staging/$package &>/dev/null; then
-        upload $package
+    if ! aws s3api head-object --bucket="${S3_BUCKET}" --key="staging/${GITSHA}/${package}" &>/dev/null; then
+        upload "${package}"
     else
-        echo "s3://$S3_BUCKET/staging/$package already exists"
+        echo "s3://${S3_BUCKET}/staging/${GITSHA}/${package} already exists"
+        aws s3 cp "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" "s3://${S3_BUCKET}/staging/$PKG"
     fi
 done
