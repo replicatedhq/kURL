ca035566c8afa9e7ba2807241d63653835b4a4e0
emosbaugh <ethan@replicated.com>
2021-04-22 06:24:23 -0700

    Copy versioned staging packages to prod (#1507)
    * Copy versioned staging packages to prod
    
    * comments

--- bin/upload-dist-prod.sh
+++ bin/upload-dist-prod.sh
@@ -13,21 +13,49 @@ require AWS_ACCESS_KEY_ID "${AWS_ACCESS_KEY_ID}"
 require AWS_SECRET_ACCESS_KEY "${AWS_SECRET_ACCESS_KEY}"
 require S3_BUCKET "${S3_BUCKET}"
 
+GITSHA="$(git rev-parse HEAD)"
+
+function upload_staging() {
+    local package="$1"
+
+    make "dist/${package}"
+    MD5="$(openssl md5 -binary "dist/${package}" | base64)"
+    aws s3 cp "dist/${package}" "s3://${S3_BUCKET}/staging/${GITSHA}/$PKG" \
+        --metadata md5="${MD5}",gitsha="${GITSHA}"
+    make clean
+    if [ -n "$DOCKER_PRUNE" ]; then
+        docker system prune --all --force
+    fi
+}
+
+# build and upload missing staging packages
 for package in $(bin/list-all-packages.sh)
 do
-    if [ "$package" = "common.tar.gz" ]; then
+    if ! aws s3api head-object --bucket="${S3_BUCKET}" --key="staging/${GITSHA}/${package}" &>/dev/null; then
+        upload_staging "${package}"
+    else
+        echo "s3://${S3_BUCKET}/staging/${GITSHA}/${package} already exists"
+    fi
+done
+
+for package in $(bin/list-all-packages.sh)
+do
+    if [ "$package" = "common.tar.gz" ] || echo "${package}" | grep -q "kurl-bin-utils" ; then
         # Common must be built rather than copied from staging because the staging common.tar.gz
         # package includes the alpha kurl-util image but the prod common.tar.gz needs a tagged version
         # of the kurl-util image
-        make dist/common.tar.gz
-        aws s3 cp dist/common.tar.gz s3://$S3_BUCKET/dist/
-    elif echo "$package" | grep -q "kurl-bin-utils"; then
+
         # The kurl-utils-bin package must be built rather than copied from staging because the staging
         # version is latest and the prod version is tagged.
-        make dist/$package
-        aws s3 cp dist/$package s3://$S3_BUCKET/dist/
+
+        make "dist/${package}"
+        MD5="$(openssl md5 -binary "dist/${package}" | base64)"
+        aws s3 cp "dist/${package}" "s3://${S3_BUCKET}/dist/${GITSHA}/${package}" \
+            --metadata md5="${MD5}",gitsha="${GITSHA}"
+        aws s3 cp "s3://${S3_BUCKET}/dist/${GITSHA}/${package}" "s3://${S3_BUCKET}/dist/${package}"
     else
-        # All other packages are copied directly from staging/ to dist/
-        aws s3 cp s3://$S3_BUCKET/staging/$package s3://$S3_BUCKET/dist/$package
+        # copy staging package to prod
+        aws s3 cp "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" "s3://${S3_BUCKET}/dist/${GITSHA}/${package}"
+        aws s3 cp "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" "s3://${S3_BUCKET}/dist/${package}"
     fi
 done
