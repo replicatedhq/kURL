4b5c885f9f726a2c99fe062ded3a045970385104
Ethan Mosbaugh <ethan@replicated.com>
2021-04-23 16:14:09 +0000

    No longer version staging packages

--- bin/upload-dist-staging.sh
+++ bin/upload-dist-staging.sh
@@ -19,7 +19,7 @@ require S3_BUCKET "${S3_BUCKET}"
 GITSHA="$(git rev-parse HEAD)"
 
 function package_has_changes() {
-    local package="$1"
+    local key="$1"
     local path="$2"
 
     if [ -z "${path}" ]; then
@@ -28,7 +28,7 @@ function package_has_changes() {
     fi
 
     local upstream_gitsha=
-    upstream_gitsha="$(aws s3api head-object --bucket "${S3_BUCKET}" --key "staging/${package}" | grep '"gitsha":' | sed 's/[",:]//g' | awk '{print $2}')"
+    upstream_gitsha="$(aws s3api head-object --bucket "${S3_BUCKET}" --key "${key}" | grep '"gitsha":' | sed 's/[",:]//g' | awk '{print $2}')"
 
     if [ -z "${upstream_gitsha}" ]; then
         # if package doesn't exist or have a gitsha it has changes
@@ -42,14 +42,12 @@ function package_has_changes() {
     fi
 }
 
-function upload() {
+function build_and_upload() {
     local package="$1"
 
     make "dist/${package}"
     MD5="$(openssl md5 -binary "dist/${package}" | base64)"
-    aws s3 cp "dist/${package}" "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" \
-        --metadata md5="${MD5}",gitsha="${GITSHA}"
-    aws s3 cp "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" "s3://${S3_BUCKET}/staging/${package}" \
+    aws s3 cp "dist/${package}" "s3://${S3_BUCKET}/staging/${package}" \
         --metadata md5="${MD5}",gitsha="${GITSHA}"
     make clean
     if [ -n "$DOCKER_PRUNE" ]; then
@@ -61,32 +59,27 @@ function deploy() {
     local package="$1"
     local path="$2"
 
-    if ! aws s3api head-object --bucket="${S3_BUCKET}" --key="staging/${GITSHA}/${package}" &>/dev/null; then
-        if package_has_changes "${package}" "${path}" ; then
-            echo "s3://${S3_BUCKET}/staging/${package} has changes"
-            upload "${package}"
-        else
-            echo "s3://${S3_BUCKET}/staging/${package} no changes in package"
-            local md5=
-            md5="$(aws s3api head-object --bucket "${S3_BUCKET}" --key "staging/${package}" | grep '"md5":' | sed 's/[",:]//g' | awk '{print $2}')"
-            aws s3 cp "s3://${S3_BUCKET}/staging/${package}" "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" \
-                --metadata md5="${md5}",gitsha="${GITSHA}"
-        fi
+    # always upload small packages that change often
+    if [ "$package" = "common.tar.gz" ] ; then
+        echo "s3://${S3_BUCKET}/${package} build and upload"
+        build_and_upload "${package}"
+        return
+    elif echo "${package}" | grep -q "kurl-bin-utils" ; then
+        echo "s3://${S3_BUCKET}/${package} build and upload"
+        build_and_upload "${package}"
+        return
+    fi
+
+    if package_has_changes "staging/${package}" "${path}" ; then
+        echo "s3://${S3_BUCKET}/staging/${package} has changes"
+        build_and_upload "${package}"
     else
-        echo "s3://${S3_BUCKET}/staging/${GITSHA}/${package} already exists"
-        local md5=
-        md5="$(aws s3api head-object --bucket "${S3_BUCKET}" --key "staging/${GITSHA}/${package}" | grep '"md5":' | sed 's/[",:]//g' | awk '{print $2}')"
-        aws s3 cp "s3://${S3_BUCKET}/staging/${GITSHA}/${package}" "s3://${S3_BUCKET}/staging/${package}" \
-            --metadata md5="${md5}",gitsha="${GITSHA}"
+        echo "s3://${S3_BUCKET}/staging/${package} no changes in package"
     fi
 }
 
 function main() {
     git fetch
-
-    # always upload small packages that change often
-    upload common.tar.gz
-    upload kurl-bin-utils-latest.tar.gz
 
     # TODO: kubernetes changes do not yet take into account changes in bundles/
     # These need to manually be rebuilt when changing that path.
