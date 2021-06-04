e31966a1c0947455ef41202df0405cbd0876954b
Andrew Reed <andrew@replicated.com>
2021-06-04 22:11:42 +0000

    Improve tests

--- kurl_util/cmd/toml/main_test.go
+++ kurl_util/cmd/toml/main_test.go
@@ -36,9 +36,6 @@ func TestMerge(t *testing.T) {
 	base := `
 version = 2
 
-[debug]
-  level = ""
-
 [timeouts]
   "io.containerd.timeout.shim.cleanup" = "5s"
   "io.containerd.timeout.shim.load" = "5s"
@@ -71,9 +68,31 @@ version = 2
 		t.Fatal(err)
 	}
 
-	merge(baseTree, patchTree)
+	expect := `version = 2
 
-	assert.Equal(t, "warn", baseTree.GetPath([]string{"debug", "level"}))
-	assert.Equal(t, true, baseTree.GetPath([]string{"plugins", "io.containerd.grpc.v1.cri", "systemd_cgroup"}))
-	assert.Equal(t, "io.containerd.runc.v2", baseTree.GetPath([]string{"plugins", "io.containerd.grpc.v1.cri", "containerd", "runtimes", "runc", "runtime_type"}))
+[debug]
+  level = "warn"
+
+[plugins]
+
+  [plugins."io.containerd.grpc.v1.cri"]
+    systemd_cgroup = true
+
+    [plugins."io.containerd.grpc.v1.cri".containerd]
+
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+
+[timeouts]
+  "io.containerd.timeout.shim.cleanup" = "5s"
+  "io.containerd.timeout.shim.load" = "5s"
+  "io.containerd.timeout.shim.shutdown" = "3s"
+  "io.containerd.timeout.task.state" = "2s"
+`
+
+	out := merge(baseTree, patchTree)
+
+	assert.Equal(t, expect, out)
 }
