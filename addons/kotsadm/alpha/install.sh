ddb886f0f27eebbb33e844c11cb43f92090d5095
Salah Aldeen Al Saleh <salahalsaleh1993@gmail.com>
2021-07-08 11:43:24 -0700

    kotsadm v1.47.0-beta.0 (#1837)

--- addons/kotsadm/alpha/install.sh
+++ addons/kotsadm/alpha/install.sh
@@ -362,7 +362,7 @@ function kotsadm_cli() {
     fi
     if [ ! -f "$src/assets/kots.tar.gz" ] && [ "$AIRGAP" != "1" ]; then
         mkdir -p "$src/assets"
-        curl -L "https://github.com/replicatedhq/kots/releases/download/v1.46.0/kots_linux_amd64.tar.gz" > "$src/assets/kots.tar.gz"
+        curl -L "https://github.com/replicatedhq/kots/releases/download/v1.47.0-beta.0/kots_linux_amd64.tar.gz" > "$src/assets/kots.tar.gz"
     fi
 
     pushd "$src/assets"
