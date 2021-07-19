0c4c1acafbba38d3b43e3712487df46c75670185
Kyle Sorensen <kyles@replicated.com>
2021-07-19 21:55:32 +0000

    adding a test version and a change to test it

--- addons/containerd/1.4.6/install.sh
+++ addons/containerd/1.4.6/install.sh
@@ -24,7 +24,7 @@ function containerd_install() {
     containerd_configure
 
     systemctl enable containerd
-
+    # test
     containerd_configure_ctl "$src"
 
     # NOTE: this will not remove the proxy
