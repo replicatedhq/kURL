187d1bd85b76d7aeba7966b329f1d35c0b1ecc73
Kyle Sorensen <kyles@replicated.com>
2021-08-10 20:49:59 +0000

    update versions, remove spot, add persistent

--- testgrid/deploy/main.tf
+++ testgrid/deploy/main.tf
@@ -26,29 +26,3 @@ output "device_ids" {
   value = metal_device.device.*.id
 }
 
-resource "metal_spot_market_request" "spot_req" {
-  count = var.device_count_spot > 0 ? 1 : 0
-
-  project_id    = var.project_id
-  max_bid_price = var.max_bid
-  facilities    = var.spot_region
-  devices_min   = min(1, var.device_count_spot)
-  devices_max   = max(1, var.device_count_spot)
-
-  instance_parameters {
-    hostname         = "${var.tg_hostname}-spot"
-    billing_cycle    = "hourly"
-    operating_system = var.tg_os
-    plan             = var.instance_type
-    userdata         = file("tg-script.sh")
-  }
-}
-
-data "metal_spot_market_request" "spot_dreq" {
-  count      = var.device_count_spot > 0 ? 1 : 0
-  request_id = element(metal_spot_market_request.spot_req, count.index).id
-}
-
-output "device_ids_spot" {
-  value = flatten(data.metal_spot_market_request.spot_dreq.*.device_ids)
-}
