de4404c1d2524ed12ed7f8d8c6adfad9219166a3
Andrew Lavery <laverya@umich.edu>
2021-08-19 13:45:48 -0400

    better logging of docker removal

--- scripts/common/docker.sh
+++ scripts/common/docker.sh
@@ -86,7 +86,7 @@ function uninstall_docker() {
     # data as possible now
     docker system prune --all --volumes --force
 
-    echo "Uninstalling Docker..."
+    logStep "Uninstalling Docker..."
     case "$LSB_DIST" in
         ubuntu)
             export DEBIAN_FRONTEND=noninteractive
@@ -103,6 +103,7 @@ function uninstall_docker() {
     rm -f /var/run/docker.sock
     echo "Docker successfully uninstalled."
 
+    echo "Starting kubelet"
     systemctl start kubelet
 
     # With the internal loadbalancer it may take a minute or two after starting kubelet before
