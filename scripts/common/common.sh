af82b68ee5ad49b8141847ce97dd85b87f8db235
garcialuis <luis@replicated.com>
2022-03-16 09:06:38 -0700

    Change ownership and permissions of admin.conf if CIS_COMPLIANCE flag is set

--- scripts/common/common.sh
+++ scripts/common/common.sh
@@ -327,11 +327,17 @@ exportKubeconfig() {
     local kubeconfig
     kubeconfig="$(${K8S_DISTRO}_get_kubeconfig)"
 
-    current_user_sudo_group
-    if [ -n "$FOUND_SUDO_GROUP" ]; then
-        chown root:$FOUND_SUDO_GROUP ${kubeconfig}
+    if [ "$CIS_COMPLIANCE" == "1" ]; then
+        chown root:root ${kubeconfig}
+        chmod 444 ${kubeconfig}
+    else
+        current_user_sudo_group
+        if [ -n "$FOUND_SUDO_GROUP" ]; then
+            chown root:$FOUND_SUDO_GROUP ${kubeconfig}
+        fi
+        chmod 440 ${kubeconfig}
     fi
-    chmod 440 ${kubeconfig}
+    
     if ! grep -q "kubectl completion bash" /etc/profile; then
         echo "export KUBECONFIG=${kubeconfig}" >> /etc/profile
         echo "source <(kubectl completion bash)" >> /etc/profile
