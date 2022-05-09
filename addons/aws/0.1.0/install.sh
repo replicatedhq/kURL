4fc26ef40212fb8d53947abad1ae1f09a50b3c33
Jalaja <jalaja@replicated.com>
2022-05-09 20:38:24 +0000

    address review comments

--- addons/aws/0.1.0/install.sh
+++ addons/aws/0.1.0/install.sh
@@ -13,16 +13,22 @@ function aws() {
 }
 
 function aws_pre_init() {
-    set_node_name
+    verify_node_name
     cp "$DIR/addons/aws/0.1.0/kubeadm-cluster-config-v1beta2.yml" "$DIR/kustomize/kubeadm/init-patches/aws-kubeadm-cluster-config-v1beta2.yml"
     cp "$DIR/addons/aws/0.1.0/kubeadm-init-config-v1beta2.yml" "$DIR/kustomize/kubeadm/init-patches/aws-kubeadm-init-config-v1beta2.yml"
 }
 
 function aws_join() {
-    set_node_name
+    verify_node_name
     cp "$DIR/addons/aws/0.1.0/kubeadm-join-config-v1beta2.yaml" "$DIR/kustomize/kubeadm/join-patches/aws-kubeadm-join-config-v1beta2.yaml"
 }
 
-function set_node_name() {
-    NODE_NAME="$(hostname -f)"
+function verify_node_name() {
+    if [ "$(hostname -f)" != "$(hostname)" ]; then
+        logFail "Hostname $(hostname -f) is different from fqdn $(hostname)"
+        printf "Continue? "
+        if ! confirmN ; then
+            bail "aws addon install is aborted."
+        fi
+    fi
 }
