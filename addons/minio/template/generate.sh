52c7164aec23a3062696e1d9f35f57469278cf51
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2022-06-14 23:36:39 +0000

    make it work

--- addons/minio/template/generate.sh
+++ addons/minio/template/generate.sh
@@ -10,18 +10,21 @@ function get_latest_version() {
 }
 
 function add_as_latest() {
-    sed -i "/cron-minio-update/a\    \"${VERSION}\"\," ../../../web/src/installers/versions.js
+    sed -i "/cron-minio-update/a\    \"${VERSION#"RELEASE."}\"\," ../../../web/src/installers/versions.js
 }
 
 function generate() {
-    local dir="../${VERSION#"RELEASE."}"
+    local dir_name=${VERSION#"RELEASE."}
+    local dir="../${dir_name}"
 
     mkdir -p "$dir"
-
     cp -r base/* "$dir/"
+
     sed -i "s/__MINIO_VERSION__/$VERSION/g" "$dir/Manifest"
     sed -i "s/__MINIO_VERSION__/$VERSION/g" "$dir/deployment.yaml"
     sed -i "s/__MINIO_VERSION__/$VERSION/g" "$dir/install.sh"
+
+    sed -i "s/__MINIO_DIR_NAME__/$dir_name/g" "$dir/install.sh"
 }
 
 function main() {
