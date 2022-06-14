12a3f8915d794148de04ede92bcb6bb61f05ffb5
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2022-06-14 23:16:34 +0000

    Automate adding new minio versions

---
+++ addons/minio/template/generate.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VERSION=
+function get_latest_version() {
+    VERSION=$(curl -s https://api.github.com/repos/minio/minio/releases/latest | \
+        grep '"tag_name":' | \
+        sed -E 's/.*"([^"]+)".*/\1/')
+}
+
+function add_as_latest() {
+    sed -i "/cron-minio-update/a\    \"${VERSION}\"\," ../../../web/src/installers/versions.js
+}
+
+function generate() {
+    local dir="../${VERSION}"
+
+    mkdir -p "$dir"
+
+    cp -r base/* "$dir/"
+    sed -i "s/__MINIO_VERSION__/$VERSION/g" "$dir/Manifest"
+}
+
+function main() {
+    VERSION=${1-}
+    if [ -z "$VERSION" ]; then
+        get_latest_version
+    fi
+
+    if [ -d "../$VERSION" ]; then
+        echo "MinIO ${VERSION} add-on already exists"
+        exit 0
+    fi
+
+    generate
+
+    add_as_latest
+
+    echo "::set-output name=minio_version::$VERSION"
+}
+
+main "$@"
