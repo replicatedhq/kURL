2e3be314efca74ba55ce3f8047530620d23457bc
replicated-ci-kots <replicated-ci-kots@users.noreply.github.com>
2022-06-17 14:44:12 +0000

    Create new Kotsadm version

--- addons/kotsadm/alpha/install.sh
+++ addons/kotsadm/alpha/install.sh
@@ -425,7 +425,7 @@ function kotsadm_cli() {
     fi
     if [ ! -f "$src/assets/kots.tar.gz" ] && [ "$AIRGAP" != "1" ]; then
         mkdir -p "$src/assets"
-        curl -L "https://github.com/replicatedhq/kots/releases/download/v1.72.0/kots_linux_amd64.tar.gz" > "$src/assets/kots.tar.gz"
+        curl -L "https://github.com/replicatedhq/kots/releases/download/v2022.6.17-5f7077-nightly/kots_linux_amd64.tar.gz" > "$src/assets/kots.tar.gz"
     fi
 
     pushd "$src/assets"
