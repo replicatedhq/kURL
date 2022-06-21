cfd5daad24bff90b5792de25818173fe173f3614
Sean Rester <syntastical@users.noreply.github.com>
2022-06-21 17:38:39 -0400

    Wait for testgrid run to complete before allowing merge to main.

--- .github/actions/testgrid-checker/index.js
+++ .github/actions/testgrid-checker/index.js
@@ -2,7 +2,7 @@ import { getExecOutput, exec } from '@actions/exec'
 import { getInput } from '@actions/core'
 import { getOctokit } from '@actions/github'
 
-const octokit = github.getOctokit(getInput('GITHUB_TOKEN'));
+const octokit = getOctokit(getInput('GITHUB_TOKEN'));
 
 const pullRequests = octokit.rest.pulls.list({
   owner: 'replicatedhq',
