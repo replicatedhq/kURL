6ec1a8fc6f00649d527ced4bc41d4d1ae80a7dcd
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2022-06-21 19:35:43 +0000

    Update local-volume-provider image to v0.3.5 in the Velero add-on

--- addons/velero/1.8.1/install.sh
+++ addons/velero/1.8.1/install.sh
@@ -150,7 +150,7 @@ function velero_install() {
         $bslArgs \
         $secretArgs \
         --namespace $VELERO_NAMESPACE \
-        --plugins velero/velero-plugin-for-aws:v1.4.1,velero/velero-plugin-for-gcp:v1.4.1,velero/velero-plugin-for-microsoft-azure:v1.4.1,replicated/local-volume-provider:v0.3.4,"$KURL_UTIL_IMAGE" \
+        --plugins velero/velero-plugin-for-aws:v1.4.1,velero/velero-plugin-for-gcp:v1.4.1,velero/velero-plugin-for-microsoft-azure:v1.4.1,replicated/local-volume-provider:v0.3.5,"$KURL_UTIL_IMAGE" \
         --use-volume-snapshots=false \
         --dry-run -o yaml > "$dst/velero.yaml" 
 
