249a102c08068843bb98320be11b83b5540f4b38
Sean Rester <syntastical@users.noreply.github.com>
2022-06-22 14:05:23 -0400

    Wait for testgrid run to complete before allowing merge to main.

--- .github/actions/testgrid-checker/index.js
+++ .github/actions/testgrid-checker/index.js
@@ -1,14 +1,51 @@
-import { getExecOutput, exec } from '@actions/exec'
 import { getInput } from '@actions/core'
-import { getOctokit } from '@actions/github'
+import { getOctokit, context } from '@actions/github'
+import { HttpClient } from '@actions/http-client'
+
+// console.log("::group::context\n", JSON.stringify(context), "\n::endgroup::");
 
 const octokit = getOctokit(getInput('GITHUB_TOKEN'));
-
+const {owner, repo} = context.repo
 const pullRequests = await octokit.rest.pulls.list({
-  owner: 'replicatedhq',
-  repo: 'kurl',
+  owner,
+  repo,
   state: 'open'
 });
 
-console.log(pullRequests);
+// console.log("::group::pullRequests\n", pullRequests, "\n::endgroup::");
 
+const httpClient = new HttpClient();
+
+const pullRequestPromises = pullRequests.data.map(async pullRequest => {
+  const prNumber = pullRequest.number;
+  const response = await httpClient.get(`https://api.testgrid.kurl.sh/api/v1/runs?searchRef=pr-${prNumber}`);
+  const responseBody = JSON.parse(await response.readBody());
+
+  let passing = true;
+  if(responseBody.total === 0) {
+    // If we're blocking pull requests that don't pass the testgrid check, what about pull requests that don't need testgrid checks?
+    return;
+  } else {
+    for (const run of responseBody.runs) {
+      if (run.pending_runs > 0) {
+        console.log(`PR #${prNumber} has pending runs`);
+        return;
+      }
+      if (run.failure_count > 0) {
+        console.log(`PR #${prNumber} has failed runs`);
+        passing = false;
+        break;
+      }
+    }
+  }
+  await octokit.rest.checks.create({
+    owner,
+    repo,
+    name: 'testgrid-checker',
+    head_sha: pullRequest.head.sha,
+    status: 'completed',
+    conclusion: passing ? 'success' : 'failure',
+  });
+});
+
+await Promise.all(pullRequestPromises);
