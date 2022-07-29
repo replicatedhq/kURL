bc85274d6edcebc57ee34c30a7aa89e53510448e
Ethan Mosbaugh <ethan@replicated.com>
2022-07-29 12:32:22 +0000

    Testgrid additional nodes must wait until test is done

--- testgrid/tgrun/pkg/runner/vmi/embed/primarynodecmd.sh
+++ testgrid/tgrun/pkg/runner/vmi/embed/primarynodecmd.sh
@@ -2,7 +2,7 @@
 
 source /opt/kurl-testgrid/common.sh
 
-function runJoinCommand() 
+function runJoinCommand()
 {
   joinCommand=$(get_join_command)
   primaryJoin=$(echo "$joinCommand" | sed 's/{.*primaryJoin":"*\([0-9a-zA-Z=]*\)"*,*.*}/\1/' | base64 -d)
@@ -30,21 +30,28 @@ function main()
 
   green "wait for join command"
   wait_for_join_commandready
-  
+
   green "run join command"
   if [ "$(is_airgap)" = "1" ]; then
-    runAirgapJoinCommand 
+    runAirgapJoinCommand
   else
     runJoinCommand
   fi
-  
+
   if [ $KURL_EXIT_STATUS -ne 0 ]; then
     report_status_update "failed"
     send_logs
     exit 1
   fi
-  
+
   green "report success join"
+  report_status_update "joined"
+  send_logs
+
+  # must stick around as part of the cluster until the test is complete
+  green "wait for initprimary done"
+  wait_for_initprimary_done
+
   report_status_update "success"
   send_logs
 }
