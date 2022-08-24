ad661ba64d9dd70a4490b4f3bb9c7b401356e663
Rafael Polanco <6497491+rrpolanco@users.noreply.github.com>
2022-08-23 22:21:53 -0500

    By default run master and worker kube-bench jobs

--- bin/run-kube-bench-k8s.sh
+++ bin/run-kube-bench-k8s.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-MASTER_NODE=$1
+NODE=$1
+
+function horizontal_rule {
+  printf "%$(tput cols)s\n"|tr " " "-"
+}
 
 job_name=""
-if [[ $MASTER_NODE == "master" ]]; then
-    job_name="kube-bench-master"
-   echo "Running job-master.yaml from kube-bench repo"
-   curl -L https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-master.yaml | kubectl create -f -
+if [[ $NODE == "master" ]]; then
+  job_name="kube-bench-master"
+  echo "Running job-master.yaml from kube-bench repo"
+  curl -Ls https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-master.yaml | kubectl create -f -
+  horizontal_rule
+elif [[ $NODE == "worker" ]]; then
+  job_name="kube-bench-node"
+  echo "Running job-node.yaml from kube-bench repo"
+  curl -Ls https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-node.yaml | kubectl create -f -
+  horizontal_rule
 else
-    job_name="kube-bench-node"
-   echo "Running job-node.yaml from kube-bench repo"
-   curl -L https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-node.yaml | kubectl create -f -
+  job_name="kube-bench-master kube-bench-node"
+  echo "Running CIS kubernetes benchmark test for master and worker nodes"
+  curl -Ls https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-master.yaml | kubectl create -f -
+  curl -Ls https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job-node.yaml | kubectl create -f -
+  horizontal_rule
 fi
 
-# wait for job to finish
-until kubectl get jobs $job_name -o jsonpath='{.status.conditions[?(@.type=="Complete")].status}' | grep -q True ; do sleep 1 ; done
+for job in $job_name; do
+  # wait for job to finish
+  until kubectl get job "$job" -o jsonpath='{.status.conditions[?(@.type=="Complete")].status}' | grep -q True ; do sleep 1 ; done
+  # dump logs
+  kubectl logs job/"$job"
+done
 
-# dump logs
-kubectl logs job/$job_name
+horizontal_rule
+echo "kube-bench test completed."
+
