69528a6cae38580828b4ab308b07c7197ff5a607
Ethan Mosbaugh <ethan@replicated.com>
2022-08-31 19:05:54 +0000

    Fix test-addon-pr.sh testgrid run

--- bin/test-addon-pr.sh
+++ bin/test-addon-pr.sh
@@ -141,7 +141,7 @@ function test_addon() {
   ref="${prefix}-${addon}-${version}-$(basename "$test_spec" ".yaml")-$(date --utc +%FT%TZ)"
 
   # Run testgrid plan
-  docker run --rm -e TESTGRID_API_TOKEN -v `pwd`:/wrk -w /wrk \
+  docker run --rm -e TESTGRID_API_TOKEN -v `pwd`:/wrk -v /tmp/test-spec:/tmp/test-spec -w /wrk \
     replicated/tgrun:latest queue --staging \
       --ref "$ref" \
       --spec /tmp/test-spec \
