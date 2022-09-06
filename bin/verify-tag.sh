7621929bf434e92b17e2267507285b91a5d0569a
Jalaja <jalaja@replicated.com>
2022-09-06 20:50:51 +0000

    Make sure if the release number is present in the tag

--- bin/verify-tag.sh
+++ bin/verify-tag.sh
@@ -14,9 +14,12 @@ function bail() {
 function main() {
     VERSION_TAG=$1
     tag_arr=(${VERSION_TAG//-/ })
+    if [ "${#tag_arr[@]}" != 2 ]; then
+        bail "Tag must have a release sequence, <vYYYY.MM.DD-[0..n]>"
+    fi
     todays_date=$(date +'v%Y.%m.%d')
     if [ "${todays_date}" != "${tag_arr[0]}" ]; then
-        bail "Tag must have today's date suffixed with a running number, <vYYYY.MM.DD-[0..n]>"
+        bail "Tag must have today's date suffixed with a release sequence, <vYYYY.MM.DD-[0..n]>"
     fi
 }
 
