8409fb009576b20d868225a95dff07cb68e2e18a
Andrew Lavery <laverya@umich.edu>
2022-09-07 23:17:45 +0200

    wait for deployments to be ready before completion
    skip upgrades from v1.0.3

--- addons/cert-manager/1.9.1/install.sh
+++ addons/cert-manager/1.9.1/install.sh
@@ -5,5 +5,24 @@ cert-manager() {
   cp "$src/cert-manager.yaml" "$dst"
   cp "$src/kustomization.yaml" "$dst"
 
+
+  # check if the current cert-manager version is v1.0.3 - if it is, skip the upgrade
+  if kubectl get ns | grep -q cert-manager; then
+      local certManagerImage=
+      certManagerImage=$(kubectl get deployment -n cert-manager cert-manager -o jsonpath='{.spec.template.spec.containers[0].image}')
+      if [ "$certManagerImage" == "quay.io/jetstack/cert-manager-controller:v1.0.3" ]; then
+          printf "not upgrading cert-manager from v1.0.3 at this time\n"
+          return 0
+      fi
+  fi
+
   kubectl apply -k  "$dst"
+
+  # wait for deployments to be ready
+  printf "awaiting cert-manager deployment"
+  spinner_until 120 deployment_fully_updated cert-manager cert-manager
+  printf "awaiting cert-manager-cainjector deployment"
+  spinner_until 120 deployment_fully_updated cert-manager cert-manager-cainjector
+  printf "awaiting cert-manager-webhook deployment"
+  spinner_until 120 deployment_fully_updated cert-manager cert-manager-webhook
 }
