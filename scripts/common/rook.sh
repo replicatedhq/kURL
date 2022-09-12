e0f1dac77878772b22afb352fbf8a0901b1f3765
Andrew Lavery <laverya@umich.edu>
2022-09-12 16:42:48 +0200

    fix incorrect image pull policy for csi-rbdplugin-provisioner
    this may not actually be the right place

--- scripts/common/rook.sh
+++ scripts/common/rook.sh
@@ -304,6 +304,7 @@ function rook_10_to_14() {
 
     echo "Upgrading ceph to v14.2.5"
     kubectl -n rook-ceph patch CephCluster rook-ceph --type=merge -p '{"spec": {"cephVersion": {"image": "ceph/ceph:v14.2.5-20191210"}}}'
+    kubectl patch deployment -n rook-ceph csi-rbdplugin-provisioner -p '{"spec": {"template": {"spec":{"containers":[{"name":"csi-snapshotter","imagePullPolicy":"IfNotPresent"}]}}}}'
     $DIR/bin/kurl rook wait-for-ceph-version "14.2.5"
 
     $DIR/bin/kurl rook wait-for-health
