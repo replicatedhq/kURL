f72d537ef7e099498e7e57ee97b368fd77622e94
Rafael Polanco <6497491+rrpolanco@users.noreply.github.com>
2022-10-28 09:58:45 -0500

    Prompt User When Migrating Data from Rook to OpenEBS (#3638)
    * Prompt user that data migration will occur
    
    * Add rook to openebs migration prompt to OpenEBS-3.3.0
    
    * address PR comments

--- addons/openebs/3.3.0/install.sh
+++ addons/openebs/3.3.0/install.sh
@@ -13,6 +13,7 @@ function openebs_pre_init() {
     export PREVIOUS_OPENEBS_VERSION="$(openebs_get_running_version)"
 
     openebs_bail_unsupported_upgrade
+    prompt_migrate_from_rook
 }
 
 function openebs() {
@@ -40,7 +41,7 @@ function openebs() {
     openebs_maybe_migrate_from_rook
 }
 
-# if rook-ceph is installed but is not specified in the kURL spec, migrate data from
+# if rook-ceph is installed but is not specified in the kURL spec, migrate data from 
 # rook-ceph to OpenEBS local pv hostpath
 function openebs_maybe_migrate_from_rook() {
     if [ -z "$ROOK_VERSION" ]; then
@@ -49,6 +50,37 @@ function openebs_maybe_migrate_from_rook() {
             DID_MIGRATE_ROOK_PVCS=1 # used to automatically delete rook-ceph if object store data was also migrated
         fi
     fi
+}
+
+function prompt_migrate_from_rook() {
+    local ceph_disk_usage_total
+    local rook_ceph_exec_deploy=rook-ceph-operator
+
+    if [ -z "$CURRENT_KUBERNETES_VERSION" ] || [ -z "$ROOK_VERSION" ]; then
+        # Don't prompt on fresh install
+        return 0
+    fi
+
+    if kubectl get deployment -n rook-ceph rook-ceph-tools &>/dev/null; then
+        rook_ceph_exec_deploy=rook-ceph-tools
+    fi
+    ceph_disk_usage_total=$(kubectl exec -n rook-ceph deployment/$rook_ceph_exec_deploy -- ceph df | grep TOTAL | awk '{ print $8$9 }')
+
+    printf "${YELLOW}"
+    printf "\n"
+    printf "    Detected Rook is running in the cluster. Data migration will be initiated to move data from rook-ceph to storage class %s.\n" "$OPENEBS_LOCALPV_STORAGE_CLASS"
+    printf "\n"
+    printf "    As part of this, all pods mounting PVCs will be stopped, taking down the application.\n"
+    printf "\n"
+    printf "    Copying the data currently stored within rook-ceph will require at least %s of free space across the cluster.\n" "$ceph_disk_usage_total"
+    printf "    It is recommended to take a snapshot or otherwise back up your data before proceeding.\n${NC}"
+    printf "\n"
+    printf "Would you like to continue? "
+
+    if ! confirmN; then
+        bail "Not migrating"
+    fi
+
 }
 
 function openebs_apply_crds() {
