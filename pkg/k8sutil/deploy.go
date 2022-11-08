a46d9926df08eca301c6c6da5149e0c70e5bbb98
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2022-11-08 11:05:24 +0100

    chore: using labels to select pods for deployment
    old habits die hard. using labels to select the pods for a given
    deployment is way more efficient.

--- pkg/k8sutil/deploy.go
+++ pkg/k8sutil/deploy.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -17,34 +17,14 @@ func DeploymentPods(ctx context.Context, cli kubernetes.Interface, ns, depname s
 		return nil, fmt.Errorf("failed to get deployment: %w", err)
 	}
 
-	rss, err := cli.AppsV1().ReplicaSets(ns).List(ctx, metav1.ListOptions{})
+	pods, err := cli.CoreV1().Pods(ns).List(
+		ctx, metav1.ListOptions{
+			LabelSelector: labels.SelectorFromSet(deploy.Spec.Selector.MatchLabels).String(),
+		},
+	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get replicaset: %w", err)
+		return nil, fmt.Errorf("failed to list pods for deploy: %w", err)
 	}
 
-	var chrs *appsv1.ReplicaSet
-	for _, rs := range rss.Items {
-		if !OwnedBy(rs.OwnerReferences, deploy.ObjectMeta) {
-			continue
-		}
-		chrs = &rs
-		break
-	}
-	if chrs == nil {
-		return nil, fmt.Errorf("failed to find replicaset for deploy: %s/%s", ns, depname)
-	}
-
-	pods, err := cli.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to get pods: %w", err)
-	}
-
-	result := []corev1.Pod{}
-	for _, pod := range pods.Items {
-		if !OwnedBy(pod.OwnerReferences, chrs.ObjectMeta) {
-			continue
-		}
-		result = append(result, pod)
-	}
-	return result, nil
+	return pods.Items, nil
 }
