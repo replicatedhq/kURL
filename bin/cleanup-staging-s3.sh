96bd53d22be4221d6249fae8d4c65e96bce77958
Andrew Lavery <laverya@umich.edu>
2022-12-05 22:16:17 +0700

    also cleanup old PR files

--- bin/cleanup-staging-s3.sh
+++ bin/cleanup-staging-s3.sh
@@ -8,10 +8,20 @@ require S3_BUCKET "${S3_BUCKET}"
 
 monthAgo=$(date --date "-744 hour" "+%s")
 
+echo "cleaning up old staging releases"
 # get the objects inside versioned staging buckets
 # then filter it for objects with timestamps older than 31 days
 # and then delete those objects older than 31 days
 aws s3api list-objects --bucket "$S3_BUCKET" --prefix 'staging/v20' --query 'Contents[].{Key: Key, LastModified: LastModified}' | \
+    jq "map(select(.LastModified | .[0:19] + \"Z\" | fromdateiso8601 < $monthAgo)) | .[].Key" | \
+    xargs -I {} echo "{}"
+#    xargs -I {} aws s3api delete-object --bucket "$S3_BUCKET" --key "{}"
+
+echo "cleaning up old PR files"
+# get the objects inside the PR folder
+# then filter it for objects with timestamps older than 31 days
+# and then delete those objects older than 31 days
+aws s3api list-objects --bucket "$S3_BUCKET" --prefix 'pr' --query 'Contents[].{Key: Key, LastModified: LastModified}' | \
     jq "map(select(.LastModified | .[0:19] + \"Z\" | fromdateiso8601 < $monthAgo)) | .[].Key" | \
     xargs -I {} echo "{}"
 #    xargs -I {} aws s3api delete-object --bucket "$S3_BUCKET" --key "{}"
