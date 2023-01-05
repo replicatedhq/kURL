c304dfb61493fa8dc76f9215bfdcda9dcd53222e
Camila Macedo <camila@replicated.com>
2023-01-05 15:09:33 +0000

    fix: add timeouts for rook upgrade from 1.0.4 to fail showing the error

--- addons/rookupgrade/10to14/install.sh
+++ addons/rookupgrade/10to14/install.sh
@@ -2,14 +2,18 @@
 
 function rookupgrade_10to14_upgrade() {
     local from_version="$1"
-
     local upgrade_files_path="$DIR/addons/rookupgrade/10to14"
 
     # if it is less than or equal we re-apply in cause of a failure mid upgrade
     if [ "$(rook_upgrade_compare_rook_versions "$from_version" "1.1")" != "1" ]; then
         # this will start the rook toolbox if it doesn't already exist
-        "$DIR"/bin/kurl rook wait-for-health
+        log "Waiting for rook to be healthy"
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
+        log "Updating the Ceph mon count"
         # If mon count is less than actual count, update mon count to actual count. Otherwise
         # updating to the latest CRDs may reduce the mon count, as preferredCount has been removed
         # from the CRD in Rook 1.1.
@@ -25,16 +29,25 @@ function rookupgrade_10to14_upgrade() {
             local actual_mon_count=
             actual_mon_count="$(kubectl -n rook-ceph exec deploy/rook-ceph-operator -- ceph mon stat | grep -o '[0-9]* mons* at' | awk '{ print $1 }')"
             if [ -n "$actual_mon_count" ] && [ "$mon_count" -lt "$actual_mon_count" ]; then
-                echo "Updating mon count to match actual mon count $actual_mon_count"
+                log "Updating mon count to match actual mon count $actual_mon_count"
                 kubectl -n rook-ceph patch cephcluster rook-ceph --type merge -p '{"spec":{"mon":{"count":'"$actual_mon_count"'}}}'
-                "$DIR"/bin/kurl rook wait-for-health
+                if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+                    kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+                    bail "Failed to verify the updated cluster, Ceph is not healthy"
+                fi
             fi
         fi
 
+        log "Preparing migration"
         "$DIR"/bin/kurl rook hostpath-to-block
-        "$DIR"/bin/kurl rook wait-for-health
 
-        echo "Rescaling pgs per pool"
+        log "Waiting for Rook to be healthy"
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
+
+        log "Rescaling pgs per pool"
         # enabling autoscaling at this point doesn't scale down the number of PGs sufficiently in my testing - it will be enabled after installing 1.4.9
         local osd_pools=
         local non_osd_pools=
@@ -42,12 +55,16 @@ function rookupgrade_10to14_upgrade() {
         non_osd_pools="$(kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph osd pool ls | grep -v rook-ceph-store)"
         rookupgrade_10to14_scale_down_osd_pool_pg_num "$osd_pools" 16
         rookupgrade_10to14_scale_down_osd_pool_pg_num "$non_osd_pools" 32
+
         log "Waiting for pool pgs to scale down"
         rookupgrade_10to14_osd_pool_wait_for_pg_num "$osd_pools" 16
         rookupgrade_10to14_osd_pool_wait_for_pg_num "$non_osd_pools" 32
 
         logStep "Upgrading to Rook 1.1.9"
-        "$DIR"/bin/kurl rook wait-for-health
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
         # first update rbac and other resources for 1.1
         kubectl create --save-config -f "$upgrade_files_path/upgrade-from-v1.0-create.yaml" || true # resources may already be present
@@ -59,25 +76,38 @@ function rookupgrade_10to14_upgrade() {
         kubectl delete crd --ignore-not-found volumesnapshotclasses.snapshot.storage.k8s.io volumesnapshotcontents.snapshot.storage.k8s.io volumesnapshots.snapshot.storage.k8s.io
         kubectl -n rook-ceph set image deploy/rook-ceph-operator rook-ceph-operator=rook/ceph:v1.1.9
         kubectl -n rook-ceph set image deploy/rook-ceph-tools rook-ceph-tools=rook/ceph:v1.1.9
-        echo "Waiting for Rook 1.1.9 to rollout throughout the cluster, this may take some time"
-        "$DIR"/bin/kurl rook wait-for-rook-version "v1.1.9"
-        # todo make sure that the RGW isn't getting stuck
-        echo "Rook 1.1.9 has been rolled out throughout the cluster"
 
-        echo "Upgrading CRDs to Rook 1.1"
+        log "Waiting for Rook 1.1.9 to rollout throughout the cluster, this may take some time"
+        if ! "$DIR"/bin/kurl rook wait-for-rook-version "v1.1.9" --timeout=1200 ; then
+            logWarn "Detected multiple Rook versions"
+            kubectl -n rook-ceph get deployment -l rook_cluster=rook-ceph -o jsonpath='{range .items[*]}{.metadata.name}{"  \treq/upd/avl: "}{.spec.replicas}{"/"}{.status.updatedReplicas}{"/"}{.status.readyReplicas}{"  \trook-version="}{.metadata.labels.rook-version}{"\n"}{end}'
+            bail "Failed to verify the Rook upgrade, multiple Rook versions detected"
+        fi
+
+        # todo make sure that the RGW isn't getting stuck
+        log "Rook 1.1.9 has been rolled out throughout the cluster"
+
+        log "Upgrading CRDs to Rook 1.1"
         kubectl apply -f "$upgrade_files_path/upgrade-from-v1.0-crds.yaml"
 
         semverCompare "$(current_ceph_version)" "14.2.5"
         if [ "$SEMVER_COMPARE_RESULT" != "1" ]; then
-            echo "Upgrading to Ceph 14.2.5"
+            log "Upgrading to Ceph 14.2.5"
 
             kubectl -n rook-ceph patch CephCluster rook-ceph --type=merge -p '{"spec": {"cephVersion": {"image": "ceph/ceph:v14.2.5-20201116"}}}'
             kubectl patch deployment -n rook-ceph csi-rbdplugin-provisioner -p '{"spec": {"template": {"spec":{"containers":[{"name":"csi-snapshotter","imagePullPolicy":"IfNotPresent"}]}}}}'
-            "$DIR"/bin/kurl rook wait-for-ceph-version "14.2.5"
+            if ! "$DIR"/bin/kurl rook wait-for-ceph-version "14.2.5" --timeout=1200 ; then
+                logWarn "Ceph version not yet rolled out"
+                kubectl -n rook-ceph get deployment -l rook_cluster=rook-ceph -o jsonpath='{range .items[*]}{.metadata.name}{"  \treq/upd/avl: "}{.spec.replicas}{"/"}{.status.updatedReplicas}{"/"}{.status.readyReplicas}{"  \tceph-version="}{.metadata.labels.ceph-version}{"\n"}{end}'
+                bail "New Ceph version failed to deploy"
+            fi
 
-            "$DIR"/bin/kurl rook wait-for-health
+            if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+                kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+                bail "Failed to verify the updated cluster, Ceph is not healthy"
+            fi
 
-            echo "Upgraded to Ceph 14.2.5 successfully"
+            logSuccess "Upgraded to Ceph 14.2.5 successfully"
         fi
 
         logSuccess "Upgraded to Rook 1.1.9 successfully"
@@ -85,88 +115,133 @@ function rookupgrade_10to14_upgrade() {
 
     if [ "$(rook_upgrade_compare_rook_versions "$from_version" "1.2")" != "1" ]; then
         logStep "Upgrading to Rook 1.2.7"
-        "$DIR"/bin/kurl rook wait-for-health
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools --ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
-        echo "Updating resources for Rook 1.2.7"
+        log "Updating resources for Rook 1.2.7"
         # apply RBAC not contained in the git repo for some reason
         kubectl apply -f "$upgrade_files_path/rook-ceph-osd-rbac.yaml"
         kubectl apply -f "$upgrade_files_path/upgrade-from-v1.1-apply.yaml"
 
         kubectl -n rook-ceph set image deploy/rook-ceph-operator rook-ceph-operator=rook/ceph:v1.2.7
         kubectl -n rook-ceph set image deploy/rook-ceph-tools rook-ceph-tools=rook/ceph:v1.2.7
-        echo "Waiting for Rook 1.2.7 to rollout throughout the cluster, this may take some time"
-        "$DIR"/bin/kurl rook wait-for-rook-version "v1.2.7"
-        echo "Rook 1.2.7 has been rolled out throughout the cluster"
-        "$DIR"/bin/kurl rook wait-for-health
 
-        echo "Upgrading CRDs to Rook 1.2"
+        log "Waiting for Rook 1.2.7 to rollout throughout the cluster, this may take some time"
+        if ! "$DIR"/bin/kurl rook wait-for-rook-version "v1.2.7" --timeout=1200 ; then
+            logWarn "Detected multiple Rook versions"
+            kubectl -n rook-ceph get deployment -l rook_cluster=rook-ceph -o jsonpath='{range .items[*]}{.metadata.name}{"  \treq/upd/avl: "}{.spec.replicas}{"/"}{.status.updatedReplicas}{"/"}{.status.readyReplicas}{"  \trook-version="}{.metadata.labels.rook-version}{"\n"}{end}'
+            bail "Failed to verify the Rook upgrade, multiple Rook versions detected"
+        fi
+
+        log "Rook 1.2.7 has been rolled out throughout the cluster"
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
+
+        log "Upgrading CRDs to Rook 1.2"
         kubectl apply -f "$upgrade_files_path/upgrade-from-v1.1-crds.yaml"
 
-        "$DIR"/bin/kurl rook wait-for-health
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
         logSuccess "Upgraded to Rook 1.2.7 successfully"
     fi
 
     if [ "$(rook_upgrade_compare_rook_versions "$from_version" "1.3")" != "1" ]; then
         logStep "Upgrading to Rook 1.3.11"
-        "$DIR"/bin/kurl rook wait-for-health
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
-        echo "Updating resources for Rook 1.3.11"
+        log "Updating resources for Rook 1.3.11"
         kubectl apply -f "$upgrade_files_path/upgrade-from-v1.2-apply.yaml"
         kubectl apply -f "$upgrade_files_path/upgrade-from-v1.2-crds.yaml"
         kubectl -n rook-ceph set image deploy/rook-ceph-operator rook-ceph-operator=rook/ceph:v1.3.11
         kubectl -n rook-ceph set image deploy/rook-ceph-tools rook-ceph-tools=rook/ceph:v1.3.11
 
-        echo "Waiting for Rook 1.3.11 to rollout throughout the cluster, this may take some time"
-        "$DIR"/bin/kurl rook wait-for-rook-version "v1.3.11"
-        echo "Rook 1.3.11 has been rolled out throughout the cluster"
-        "$DIR"/bin/kurl rook wait-for-health
+        log "Waiting for Rook 1.3.11 to rollout throughout the cluster, this may take some time"
+        if ! "$DIR"/bin/kurl rook wait-for-rook-version "v1.3.11" --timeout=1200 ; then
+            logWarn "Detected multiple Rook versions"
+            kubectl -n rook-ceph get deployment -l rook_cluster=rook-ceph -o jsonpath='{range .items[*]}{.metadata.name}{"  \treq/upd/avl: "}{.spec.replicas}{"/"}{.status.updatedReplicas}{"/"}{.status.readyReplicas}{"  \trook-version="}{.metadata.labels.rook-version}{"\n"}{end}'```
+            bail "Failed to verify the Rook upgrade, multiple Rook versions detected"
+        fi
+
+        log "Rook 1.3.11 has been rolled out throughout the cluster"
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
         logSuccess "Upgraded to Rook 1.3.11 successfully"
     fi
 
     if [ "$(rook_upgrade_compare_rook_versions "$from_version" "1.4")" != "1" ]; then
         logStep "Upgrading to Rook 1.4.9"
-        "$DIR"/bin/kurl rook wait-for-health
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
-        echo "Updating resources for Rook 1.4.9"
+        log "Updating resources for Rook 1.4.9"
         kubectl delete -f "$upgrade_files_path/upgrade-from-v1.3-delete.yaml"
         kubectl apply -f "$upgrade_files_path/upgrade-from-v1.3-apply.yaml"
         kubectl apply -f "$upgrade_files_path/upgrade-from-v1.3-crds.yaml"
         kubectl -n rook-ceph set image deploy/rook-ceph-operator rook-ceph-operator=rook/ceph:v1.4.9
         kubectl apply -f "$upgrade_files_path/rook-ceph-tools-14.yaml"
 
-        echo "Waiting for Rook 1.4.9 to rollout throughout the cluster, this may take some time"
-        "$DIR"/bin/kurl rook wait-for-rook-version "v1.4.9"
-        echo "Rook 1.4.9 has been rolled out throughout the cluster"
-        "$DIR"/bin/kurl rook wait-for-health
+        log "Waiting for Rook 1.4.9 to rollout throughout the cluster, this may take some time"
+        if ! "$DIR"/bin/kurl rook wait-for-rook-version "v1.4.9" --timeout=1200 ; then
+            logWarn "Detected multiple Rook versions"
+            kubectl -n rook-ceph get deployment -l rook_cluster=rook-ceph -o jsonpath='{range .items[*]}{.metadata.name}{"  \treq/upd/avl: "}{.spec.replicas}{"/"}{.status.updatedReplicas}{"/"}{.status.readyReplicas}{"  \trook-version="}{.metadata.labels.rook-version}{"\n"}{end}'
+            bail "Failed to verify the Rook upgrade, multiple Rook versions detected"
+        fi
+
+        log "Rook 1.4.9 has been rolled out throughout the cluster"
+        if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+            kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+            bail "Failed to verify the updated cluster, Ceph is not healthy"
+        fi
 
         semverCompare "$(current_ceph_version)" "15.2.8"
         if [ "$SEMVER_COMPARE_RESULT" != "1" ]; then
-            echo "Upgrading to Ceph 15.2.8"
+            logStep "Upgrading to Ceph 15.2.8"
             kubectl -n rook-ceph patch CephCluster rook-ceph --type=merge -p '{"spec": {"cephVersion": {"image": "ceph/ceph:v15.2.8-20201217"}}}'
 
             # EKCO will scale device_health_metrics to 3 once the cluster is upgraded to Ceph
             # 15.2.8, but since we have scaled EKCO to 0 replicas it cannot, so we need to do it
             # manually.
-            echo "Waiting for device_health_metrics pool to be created"
+            log "Waiting for device_health_metrics pool to be created"
             if ! rookupgrade_10to14_wait_for_pool_device_health_metrics ; then
                 bail "Timed out waiting for device_health_metrics pool to be created"
             fi
             rookupgrade_10to14_maybe_scale_pool_device_health_metrics
 
-            "$DIR"/bin/kurl rook wait-for-ceph-version "15.2.8-0"
+            if ! "$DIR"/bin/kurl rook wait-for-ceph-version "15.2.8-0" --timeout=1200 ; then
+                logWarn "Ceph version not yet rolled out"
+                kubectl -n rook-ceph get deployment -l rook_cluster=rook-ceph -o jsonpath='{range .items[*]}{.metadata.name}{"  \treq/upd/avl: "}{.spec.replicas}{"/"}{.status.updatedReplicas}{"/"}{.status.readyReplicas}{"  \tceph-version="}{.metadata.labels.ceph-version}{"\n"}{end}'
+                bail "New Ceph version failed to deploy"
+            fi
 
-            echo "Enabling pg pool autoscaling"
+            log "Enabling pg pool autoscaling"
             kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph osd pool ls | \
             xargs -I {} kubectl -n rook-ceph exec deploy/rook-ceph-tools -- \
             ceph osd pool set {} pg_autoscale_mode on
-            echo "Current pg pool autoscaling status:"
+
+            log "Current pg pool autoscaling status:"
             kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph osd pool autoscale-status
 
-            "$DIR"/bin/kurl rook wait-for-health
+            if ! $DIR/bin/kurl rook wait-for-health 300 ; then
+                kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph status
+                bail "Failed to verify the updated cluster, Ceph is not healthy"
+            fi
 
-            echo "Upgraded to Ceph 15.2.8 successfully"
+            logSuccess "Upgraded to Ceph 15.2.8 successfully"
         fi
 
         logSuccess "Upgraded to Rook 1.4.9 successfully"
@@ -232,7 +307,7 @@ function rookupgrade_10to14_maybe_scale_pool_device_health_metrics() {
     device_health_metrics_size="$(kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph osd pool get device_health_metrics size | awk '{ print $2 }')"
     replicapool_size="$(kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph osd pool get replicapool size | awk '{ print $2 }')"
     if [ -n "$device_health_metrics_size" ] && [ -n "$replicapool_size" ] && [ "$replicapool_size" -gt 1 ] && [ "$device_health_metrics_size" -lt "$replicapool_size" ]; then
-        echo "Setting device_health_metrics pool size to $replicapool_size and min_size to 2"
+        log "Setting device_health_metrics pool size to $replicapool_size and min_size to 2"
         kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph osd pool set device_health_metrics size "$replicapool_size"
         # min_size is always 2 for greater than 1 node clusters
         kubectl -n rook-ceph exec deploy/rook-ceph-tools -- ceph osd pool set device_health_metrics min_size 2
