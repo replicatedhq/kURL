6e47b79bd9c0aedee33f800ecf5f38c763e16104
Rafael Polanco <6497491+rrpolanco@users.noreply.github.com>
2023-01-26 22:24:48 -0600

    feat(ci/cd): Batch s3 copies across parallel jobs (#3997)

--- bin/list-all-packages-actions-matrix.sh
+++ bin/list-all-packages-actions-matrix.sh
@@ -2,9 +2,6 @@
 
 set -eo pipefail
 
-# shellcheck source=list-all-packages.sh
-source ./bin/list-all-packages.sh
-
 function require() {
     if [ -z "$2" ]; then
         echo "validation failed: $1 unset"
@@ -12,17 +9,24 @@ function require() {
     fi
 }
 
-require KURL_UTIL_IMAGE "${KURL_UTIL_IMAGE}" # required for common package
-require KURL_BIN_UTILS_FILE "${KURL_BIN_UTILS_FILE}"
-
 batch_size=3
-
 i=0
 start=0
 
-package_list=$(list_all | awk '{print $1}')
-if [ "${FILTER_GO_BINS_ONLY}" == "1" ]; then
-    package_list=$(list_go_bins| awk '{print $1}')
+package_list=
+if [ "${LIST_FROM_STAGE_S3}" == "1" ]; then
+    package_list=$(./bin/list-packages-s3.sh | awk '{print $1}')
+else
+    require KURL_UTIL_IMAGE "${KURL_UTIL_IMAGE}" # required for common package
+    require KURL_BIN_UTILS_FILE "${KURL_BIN_UTILS_FILE}"
+    # shellcheck source=list-all-packages.sh
+    source ./bin/list-all-packages.sh
+    if [ "${FILTER_GO_BINS_ONLY}" == "1" ]; then
+        package_list=$(list_go_bins| awk '{print $1}')
+    else
+        # default to listing all packages
+        package_list=$(list_all | awk '{print $1}')
+    fi
 fi
 
 printf '{"include": ['
