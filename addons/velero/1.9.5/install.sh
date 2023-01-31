a0ab297d61ab65c391bc1b6a637dfc81983ecfb7
emosbaugh <emosbaugh@users.noreply.github.com>
2023-01-31 04:03:23 +0000

    Create new Velero version

--- addons/velero/1.9.5/install.sh
+++ addons/velero/1.9.5/install.sh
@@ -150,7 +150,7 @@ function velero_install() {
         $bslArgs \
         $secretArgs \
         --namespace $VELERO_NAMESPACE \
-        --plugins velero/velero-plugin-for-aws:v1.6.0,velero/velero-plugin-for-gcp:v1.6.0,velero/velero-plugin-for-microsoft-azure:v1.6.0,replicated/local-volume-provider:v0.4.3,"$KURL_UTIL_IMAGE" \
+        --plugins velero/velero-plugin-for-aws:v1.6.0,velero/velero-plugin-for-gcp:v1.6.0,velero/velero-plugin-for-microsoft-azure:v1.6.0,replicated/local-volume-provider:v0.4.4,"$KURL_UTIL_IMAGE" \
         --use-volume-snapshots=false \
         --dry-run -o yaml > "$dst/velero.yaml" 
 
