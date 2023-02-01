2b4614334656647e3dd713b678c9f8787288322c
emosbaugh <emosbaugh@users.noreply.github.com>
2023-02-01 04:05:32 +0000

    Create new minio version

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -407,6 +407,7 @@ module.exports.InstallerVersions = {
   ],
   minio: [
     // cron-minio-update
+    "2023-01-31T02-24-19Z",
     "2023-01-25T00-19-54Z",
     "2023-01-20T02-05-44Z",
     "2023-01-18T04-36-38Z",
