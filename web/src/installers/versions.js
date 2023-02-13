8120859f87a9b162d9bb421df448f00abc5ac02a
replicated-ci-kurl <91491290+replicated-ci-kurl@users.noreply.github.com>
2023-02-13 02:13:47 -0600

    Create new minio version (#4079)

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -411,6 +411,7 @@ module.exports.InstallerVersions = {
   ],
   minio: [
     // cron-minio-update
+    "2023-02-10T18-48-39Z",
     "2023-02-09T05-16-53Z",
     "2023-01-31T02-24-19Z",
     "2023-01-25T00-19-54Z",
