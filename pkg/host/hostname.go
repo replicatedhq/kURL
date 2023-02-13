215eb518799f0048b7e59eb5a27e14053c3a423a
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2023-02-13 19:22:37 +0000

    Apply suggestions from code review

--- pkg/host/hostname.go
+++ pkg/host/hostname.go
@@ -7,6 +7,8 @@ import (
 )
 
 // GetHostname returns the node hostname using kubernetes as lib to ensure that will obtain the same value
+// This code is the same used kubernetes project: (We copied to avoid to add a dependency only because of this snipt)
+//https://github.com/kubernetes/kubernetes/blob/6b34fafdaf5998039c7e01fa33920a641b216d3e/staging/src/k8s.io/component-helpers/node/util/hostname.go#L25-L46
 func GetHostname() (string, error) {
 	nodeName, err := os.Hostname()
 	if err != nil {
