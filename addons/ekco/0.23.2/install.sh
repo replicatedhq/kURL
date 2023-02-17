089416ea9cfe44472498c9e1d5e096f5a011fc15
Ethan Mosbaugh <ethan@replicated.com>
2023-02-17 19:57:36 +0000

    fix(ekco): fix error flags --detach and --rm cannot be specified together

--- addons/ekco/0.23.2/install.sh
+++ addons/ekco/0.23.2/install.sh
@@ -319,7 +319,7 @@ function ekco_bootstrap_internal_lb() {
 
         ctr -n k8s.io task kill -s SIGKILL bootstrap-lb &>/dev/null || true
         ctr -n k8s.io containers delete bootstrap-lb &>/dev/null || true
-        ctr -n k8s.io run --rm \
+        ctr -n k8s.io run \
             --mount "type=bind,src=/etc/haproxy,dst=/usr/local/etc/haproxy,options=rbind:ro" \
             --net-host \
             --detach \
--- addons/ekco/0.26.0/install.sh
+++ addons/ekco/0.26.0/install.sh
@@ -329,7 +329,7 @@ function ekco_bootstrap_internal_lb() {
 
         ctr -n k8s.io task kill -s SIGKILL bootstrap-lb &>/dev/null || true
         ctr -n k8s.io containers delete bootstrap-lb &>/dev/null || true
-        ctr -n k8s.io run --rm \
+        ctr -n k8s.io run \
             --mount "type=bind,src=/etc/haproxy,dst=/usr/local/etc/haproxy,options=rbind:ro" \
             --net-host \
             --detach \
