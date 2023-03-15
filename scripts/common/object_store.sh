442cecc588c3ef539c40b6d71201f9891e92f213
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2023-03-15 17:24:52 +0000

    Update scripts/common/object_store.sh
    Co-authored-by: Ethan Mosbaugh <ethan@replicated.com>

--- scripts/common/object_store.sh
+++ scripts/common/object_store.sh
@@ -255,7 +255,7 @@ function rook_rgw_is_healthy() {
     export OBJECT_STORE_CLUSTER_HOST="http://rook-ceph-rgw-rook-ceph-store.rook-ceph"
     # same as OBJECT_STORE_CLUSTER_IP for IPv4, wrapped in brackets for IPv6
     export OBJECT_STORE_CLUSTER_IP_BRACKETED
-    OBJECT_STORE_CLUSTER_IP_BRACKETED=$("$DIR"/bin/kurl netutil format-ip-address "$OBJECT_STORE_CLUSTER_IP")
+    OBJECT_STORE_CLUSTER_IP_BRACKETED=$("$BIN_KURL" netutil format-ip-address "$OBJECT_STORE_CLUSTER_IP")
     curl --globoff --noproxy "*" --fail --silent --insecure "http://${OBJECT_STORE_CLUSTER_IP_BRACKETED}" > /dev/null
 }
 
