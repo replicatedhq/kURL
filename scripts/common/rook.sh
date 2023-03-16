32fb0dd91c45740386374f6b3a7c2dbc980ea795
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2023-03-16 19:31:13 +0000

    Apply suggestions from code review
    Co-authored-by: Andrew Lavery <laverya@umich.edu>

--- scripts/common/rook.sh
+++ scripts/common/rook.sh
@@ -224,17 +224,13 @@ function maybe_cleanup_rook() {
         # we do not know if the solution uses or not ObjectStore and if someone data will not be lost
         if [ "$DID_MIGRATE_ROOK_PVCS" == "1" ] && [ -z "$MINIO_VERSION" ]; then
             if [ -z "$DID_MIGRATE_ROOK_OBJECT_STORE" ] || [ "$DID_MIGRATE_ROOK_OBJECT_STORE" != "1" ]; then
-                logWarn "The PVC(s) were migrated but this install does not specified a Minio version."
-                logWarn "If you are migration from Rook to OpenEBS without select an Minio version then"
-                logWarn "the installer cannot programmatically remove Rook."
+                logWarn "PVC(s) were migrated from Rook but Object Store data was not, as no MinIO version was specified."
+                logWarn "Rook will not be automatically removed without migrating Object Store data."
                 logWarn ""
-                logWarn "You can manually perform this operation by running the remove_rook_ceph task:"
+                logWarn "If you are sure that Object Store data is not used, you can manually perform this operation by running the remove_rook_ceph task:"
                 logWarn "$ curl <installer>/task.sh | sudo bash -s remove_rook_ceph, i.e.:"
                 logWarn ""
                 logWarn "curl https://kurl.sh/latest/tasks.sh | sudo bash -s remove_rook_ceph"
-                logWarn ""
-                logWarn "However, please only manually remove Rook from the host after you ensure that"
-                logWarn "the migration was successful to prevent the possibility of data loss."
             fi
         fi
         logWarn ""
