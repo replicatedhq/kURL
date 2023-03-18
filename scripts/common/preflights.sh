da1211d4edc07dd5aed59055a34886ccd1f32f28
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2023-03-18 11:33:02 +0000

    Update scripts/common/preflights.sh

--- scripts/common/preflights.sh
+++ scripts/common/preflights.sh
@@ -668,7 +668,7 @@ function bail_when_no_object_store_and_s3_enabled() {
              logFail "Velero requires an object store."
              bail "Please, ensure that your installer also provides an object store with either the MinIO or Rook add-on."
         fi
-        if [ -n "$REGISTRY_VERSION" ]  && [ "$HA_CLUSTER" = "1" ];then
+        if [ -n "$REGISTRY_VERSION" ];then
              bail "OpenEBS and Registry for HA installs requires MinIO. Please, ensure that your installer also provides MinIO"
         fi
     fi
