93e33225ef68b6186b3ed26b0573e17679237da8
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2023-03-20 19:28:15 +0000

    Update scripts/common/rook.sh

--- scripts/common/rook.sh
+++ scripts/common/rook.sh
@@ -69,7 +69,7 @@ function remove_rook_ceph() {
     if echo "$all_pv_drivers" | grep "rook" &>/dev/null ; then
         logFail "There are still PVs using rook-ceph."
         logFail "Remove these PV(s) before continuing:"
-       echo $(echo "$all_pv_drivers" | grep "rook")
+        echo $(echo "$all_pv_drivers" | grep "rook")
         return 1
     fi
 
