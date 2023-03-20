8f7ef3fb321d78ade7fec9654616e8339e4004f0
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2023-03-20 18:51:05 +0000

    Update scripts/common/rook.sh
    Co-authored-by: Ethan Mosbaugh <ethan@replicated.com>

--- scripts/common/rook.sh
+++ scripts/common/rook.sh
@@ -123,7 +123,7 @@ function remove_rook_ceph() {
 
     log "Removing rook-ceph Storage Classes"
     if ! kubectl get storageclass | grep rook | awk '{ print $1 }' | xargs -I'{}' kubectl delete storageclass '{}' --timeout=60s; then
-        logFail "Unable delete rook-ceph StorageClasses"
+        logFail "Unable to delete rook-ceph StorageClasses"
         return 1
     fi
     
