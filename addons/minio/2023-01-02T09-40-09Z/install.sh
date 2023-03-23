4a799566601735d5bc76a8caa731ffe5ff2e60c8
camilamacedo86 <camilamacedo86@gmail.com>
2023-03-23 15:26:16 +0000

    feat: now longer allow migrate the object store twice

--- addons/minio/2023-01-02T09-40-09Z/install.sh
+++ addons/minio/2023-01-02T09-40-09Z/install.sh
@@ -234,6 +234,12 @@ function minio_migrate_from_rgw() {
 
     minio_wait_for_health
 
+    DID_MIGRATE_ROOK_OBJECT_STORE=$(kubectl -n kurl get configmap kurl-migration-from-rook -o jsonpath='{ .data.DID_MIGRATE_ROOK_OBJECT_STORE }')
+    if [ "$DID_MIGRATE_ROOK_OBJECT_STORE" == "1" ]; then
+        logWarn "Object store is set as migrated previously. Not migrating object store again."
+        return
+    fi
+
     migrate_rgw_to_minio
     add_rook_store_object_migration_status
 }
