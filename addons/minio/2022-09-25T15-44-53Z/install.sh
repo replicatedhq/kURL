318bc27024547ff667ec7907a8f8e626065c0010
Camila Macedo <7708031+camilamacedo86@users.noreply.github.com>
2023-03-24 22:41:01 +0000

    Update addons/minio/2022-09-25T15-44-53Z/install.sh

--- addons/minio/2022-09-25T15-44-53Z/install.sh
+++ addons/minio/2022-09-25T15-44-53Z/install.sh
@@ -190,7 +190,6 @@ function minio_migrate_from_rgw() {
 
     minio_wait_for_health
 
-
     export DID_MIGRATE_ROOK_OBJECT_STORE=0
     DID_MIGRATE_ROOK_OBJECT_STORE=$(kubectl -n kurl get configmap kurl-migration-from-rook -o jsonpath='{ .data.DID_MIGRATE_ROOK_OBJECT_STORE }')
     if [ "$DID_MIGRATE_ROOK_OBJECT_STORE" == "1" ]; then
