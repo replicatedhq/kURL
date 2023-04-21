902034f26ab313e0bfa9d9619b940fc96b6692f8
replicated-ci-kurl <91491290+replicated-ci-kurl@users.noreply.github.com>
2023-04-21 13:13:33 -0500

    Automated Goldpinger version update 3.7.0-6.0.1 (#4363)
    * Create new Goldpinger version
    
    * simplify goldpinger tests
    
    ---------
    
    Co-authored-by: emosbaugh <emosbaugh@users.noreply.github.com>
    Co-authored-by: Andrew Lavery <laverya@umich.edu>

---
+++ addons/goldpinger/3.7.0-6.0.1/install.sh
@@ -0,0 +1,34 @@
+
+function goldpinger() {
+    local src="$DIR/addons/goldpinger/3.7.0-6.0.1"
+    local dst="$DIR/kustomize/goldpinger"
+
+    cp "$src/kustomization.yaml" "$dst/"
+    cp "$src/goldpinger.yaml" "$dst/"
+    cp "$src/servicemonitor.yaml" "$dst/"
+
+    if [ -n "${PROMETHEUS_VERSION}" ]; then
+        insert_resources "$dst/kustomization.yaml" servicemonitor.yaml
+    fi
+
+    kubectl apply -k "$dst/"
+
+    logStep "Waiting for the Goldpinger Daemonset to be ready"
+    spinner_until 180 goldpinger_daemonset
+    logStep "Waiting for the Goldpinger service to be ready"
+    spinner_until 120 kubernetes_service_healthy kurl goldpinger
+    logSuccess "Goldpinger is ready and monitoring node network health"
+}
+
+function goldpinger_daemonset() {
+    local desired=$(kubectl get daemonsets -n kurl goldpinger --no-headers | tr -s ' ' | cut -d ' ' -f2)
+    local ready=$(kubectl get daemonsets -n kurl goldpinger --no-headers | tr -s ' ' | cut -d ' ' -f4)
+    local uptodate=$(kubectl get daemonsets -n kurl goldpinger --no-headers | tr -s ' ' | cut -d ' ' -f5)
+
+    if [ "$desired" = "$ready" ] ; then
+        if [ "$desired" = "$uptodate" ] ; then
+            return 0
+        fi
+    fi
+    return 1
+}
