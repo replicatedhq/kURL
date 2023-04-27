e9664342eb887e6cd5f9f2b92d4980022c70d0c7
Rafael R. Polanco <6497491+rrpolanco@users.noreply.github.com>
2023-04-27 02:14:40 +0000

    kustomize v5 does not support multi-doc patches

--- scripts/common/yaml.sh
+++ scripts/common/yaml.sh
@@ -31,10 +31,20 @@ function insert_patches_strategic_merge() {
     # Kubernetes 1.27 uses kustomize v5 which dropped support for old, legacy style patches
     # See: https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.27.md#changelog-since-v1270
     if [ "$KUBERNETES_TARGET_VERSION_MINOR" -ge "27" ]; then
-        if ! grep -q "patches" "$kustomization_file"; then
-            echo "patches:" >> "$kustomization_file"
+        if [[ $kustomization_file =~ "prometheus" ]]; then
+            # TODO: multi-doc patches is not currently supported in kustomize v5
+            # continue using the deprecated 'patchesStrategicMerge' field until this is fixed
+            # Ref: https://github.com/kubernetes-sigs/kustomize/issues/5040
+            if ! grep -q "patchesStrategicMerge" "$kustomization_file"; then
+                echo "patchesStrategicMerge:" >> "$kustomization_file"
+            fi
+            sed -i "/patchesStrategicMerge.*/a - $patch_file" "$kustomization_file"
+        else
+            if ! grep -q "^patches:" "$kustomization_file"; then
+                echo "patches:" >> "$kustomization_file"
+            fi
+            sed -i "/patches:/a - path: $patch_file" "$kustomization_file"
         fi
-        sed -i "/patches.*/a - path: $patch_file" "$kustomization_file"
         return
     fi
 
