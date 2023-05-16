a5980663efe808f5b99d8dd59e1cbecd07ef2638
emosbaugh <emosbaugh@users.noreply.github.com>
2023-05-16 04:02:14 +0000

    Create new Prometheus version

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -198,6 +198,7 @@ module.exports.InstallerVersions = {
   ],
   prometheus: [
     // cron-prometheus-update
+    "0.65.1-45.28.0",
     "0.65.1-45.27.2",
     "0.65.1-45.27.1",
     "0.65.1-45.26.0",
