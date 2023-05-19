3c7e035640dc7a29fdcf1e2701095be0c339ba5a
replicated-ci-kurl <91491290+replicated-ci-kurl@users.noreply.github.com>
2023-05-19 09:43:21 -0500

    Automated MinIO version update RELEASE.2023-05-18T00-05-36Z (#4517)

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -467,6 +467,7 @@ module.exports.InstallerVersions = {
   ],
   minio: [
     // cron-minio-update
+    "2023-05-18T00-05-36Z",
     "2023-05-04T21-44-30Z",
     "2023-04-20T17-56-55Z",
     "2023-04-13T03-08-07Z",
