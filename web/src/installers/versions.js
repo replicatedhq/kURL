9acf2aa2ed7c7867b374d6931f2f8cfaa77a35c4
replicated-ci-kurl <91491290+replicated-ci-kurl@users.noreply.github.com>
2023-06-27 12:28:27 -0500

    Automated MinIO version update RELEASE.2023-06-23T20-26-00Z (#4641)
    Create new minio version
    
    Co-authored-by: emosbaugh <emosbaugh@users.noreply.github.com>

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -479,6 +479,7 @@ module.exports.InstallerVersions = {
   ],
   minio: [
     // cron-minio-update
+    "2023-06-23T20-26-00Z",
     "2023-06-19T19-52-50Z",
     "2023-06-09T07-32-12Z",
     "2023-05-18T00-05-36Z",
