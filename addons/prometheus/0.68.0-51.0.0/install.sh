9c8c54948a924525f32816480bc9f43ff6bd72e4
Diamon Wiggins <38189728+diamonwiggins@users.noreply.github.com>
2023-10-12 12:40:51 -0400

    Update prometheus_crd_ready function to prevent service side apply issues during upgrade (#4882)
    * Adding the fairly recently prometheusagent crd to prometheus_crd_ready function to reduce chance of ssa race condition issues
    
    * add test case for 0.49.x of Prometheus
    
    * Update addons/prometheus/template/testgrid/k8s-docker.yaml
    
    Co-authored-by: Andrew Lavery <laverya@umich.edu>
    
    * Update addons/prometheus/template/testgrid/k8s-docker.yaml
    
    Co-authored-by: Andrew Lavery <laverya@umich.edu>
    
    * Update addons/prometheus/template/testgrid/k8s-docker.yaml
    
    Co-authored-by: Andrew Lavery <laverya@umich.edu>
    
    ---------
    
    Co-authored-by: Andrew Lavery <laverya@umich.edu>

--- addons/prometheus/0.68.0-51.0.0/install.sh
+++ addons/prometheus/0.68.0-51.0.0/install.sh
@@ -117,6 +117,9 @@ function prometheus_crd_ready() {
     if ! kubectl get customresourcedefinitions servicemonitors.monitoring.coreos.com &>/dev/null; then
         return 1
     fi
+    if ! kubectl get customresourcedefinitions servicemonitors.monitoring.coreos.com -o yaml | grep "enableHttp2" &>/dev/null; then
+        return 1
+    fi
     if ! kubectl get servicemonitors --all-namespaces &>/dev/null; then
         return 1
     fi
@@ -124,6 +127,12 @@ function prometheus_crd_ready() {
         return 1
     fi
     if ! kubectl get prometheuses --all-namespaces &>/dev/null; then
+        return 1
+    fi
+    if ! kubectl get customresourcedefinitions prometheusagents.monitoring.coreos.com &>/dev/null; then
+        return 1
+    fi
+    if ! kubectl get prometheusagents --all-namespaces &>/dev/null; then
         return 1
     fi
     return 0
