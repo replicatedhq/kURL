bab17c16c6330db2151bf5d9c7bd7f738e533a87
Andrew Lavery <laverya@umich.edu>
2024-04-19 22:30:42 +0900

    Allow k8s 1.30 (#5151)
    * generate k8s 1.30 versions
    
    * update testgrid to use k8s 1.30

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -23,6 +23,7 @@ module.exports.InstallerVersions = {
     "1.17.7",
     "1.17.3",
     "1.16.4",
+    // cron-kubernetes-update-130
     // cron-kubernetes-update-129
     "1.29.4",
     "1.29.3",
