8ca047225051142154e38c6e897be92fb8ff0966
emosbaugh <371319+emosbaugh@users.noreply.github.com>
2024-06-13 04:04:09 +0000

    Create new Kubernetes version

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -24,9 +24,11 @@ module.exports.InstallerVersions = {
     "1.17.3",
     "1.16.4",
     // cron-kubernetes-update-130
+    "1.30.2",
     "1.30.1",
     "1.30.0",
     // cron-kubernetes-update-129
+    "1.29.6",
     "1.29.5",
     "1.29.4",
     "1.29.3",
