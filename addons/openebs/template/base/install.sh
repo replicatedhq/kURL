b614064fe44ce2249eef8678f48abf6d7ac5e1b1
Ethan Mosbaugh <ethan@replicated.com>
2025-08-25 09:23:06 -0700

    f

--- addons/openebs/template/base/install.sh
+++ addons/openebs/template/base/install.sh
@@ -84,7 +84,15 @@ function openebs_maybe_rook_migration_checks() {
 
     # Ensure openebs-localpv-provisioner deployment is ready
     log "awaiting openebs-localpv-provisioner deployment"
-    spinner_until 120 deployment_fully_updated openebs openebs-localpv-provisioner
+    if ! spinner_until 120 deployment_fully_updated openebs openebs-localpv-provisioner; then
+        bail "OpenEBS localpv-provisioner deployment failed to update"
+    fi
+
+    # Ensure openebs-localpv storage class is ready
+    log "awaiting openebs-localpv storage class"
+    if ! spinner_until 120 kubectl get storageclass "$OPENEBS_LOCALPV_STORAGE_CLASS" &>/dev/null; then
+        bail "OpenEBS localpv storage class failed to be created"
+    fi
 
     local rook_scs_pvmigrate_dryrun_output
     local rook_default_sc_pvmigrate_dryrun_output
