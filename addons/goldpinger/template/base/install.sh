f2658ce5523beccf6f3651976c9e5819654854aa
Diamon Wiggins <diamonw757@gmail.com>
2025-08-28 17:21:36 -0400

    ensure base template script has needed logic and simplify values.yaml

--- addons/goldpinger/template/base/install.sh
+++ addons/goldpinger/template/base/install.sh
@@ -2,6 +2,26 @@
 function goldpinger() {
     local src="$DIR/addons/goldpinger/__GOLDPINGER_VERSION__"
     local dst="$DIR/kustomize/goldpinger"
+    
+    # Check if upgrading from old okgolove chart
+    if is_old_chart; then
+        logStep "Migrating goldpinger from okgolove to Bloomberg chart..."
+        
+        # Clean uninstall of old okgolove chart resources
+        kubectl delete daemonset -n kurl goldpinger --ignore-not-found=true
+        kubectl delete configmap -n kurl goldpinger-zap --ignore-not-found=true
+        kubectl delete service -n kurl goldpinger --ignore-not-found=true
+        kubectl delete serviceaccount -n kurl goldpinger --ignore-not-found=true
+        kubectl delete clusterrole goldpinger-clusterrole --ignore-not-found=true
+        kubectl delete clusterrolebinding goldpinger-clusterrolebinding --ignore-not-found=true
+        
+        # Wait for old pods to terminate
+        kubectl wait --for=delete pod -l app.kubernetes.io/name=goldpinger -n kurl --timeout=30s || true
+        
+        logStep "Installing Bloomberg goldpinger chart..."
+    else
+        logStep "Installing goldpinger __GOLDPINGER_VERSION__..."
+    fi
 
     cp "$src/kustomization.yaml" "$dst/"
     cp "$src/goldpinger.yaml" "$dst/"
@@ -18,7 +38,14 @@ function goldpinger() {
     spinner_until 180 goldpinger_daemonset
     logStep "Waiting for the Goldpinger service to be ready"
     spinner_until 120 kubernetes_service_healthy kurl goldpinger
-    logSuccess "Goldpinger is ready and monitoring node network health"
+    logSuccess "Goldpinger __GOLDPINGER_VERSION__ is ready"
+}
+
+function is_old_chart() {
+    # Check for okgolove chart versions (4.x, 5.x, 6.x)
+    kubectl get daemonset -n kurl goldpinger \
+        -o jsonpath="{.metadata.labels['helm\.sh/chart']}" 2>/dev/null | \
+        grep -E "goldpinger-(4|5|6)\." &>/dev/null
 }
 
 function goldpinger_daemonset() {
