2517d83df81cf8a54e9bfc2b030a71c1ee093de8
laverya <2318911+laverya@users.noreply.github.com>
2025-09-08 01:30:07 +0000

    Create new minio version

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -629,6 +629,7 @@ module.exports.InstallerVersions = {
   ],
   minio: [
     // cron-minio-update
+    "2025-09-07T16-13-09Z",
     "2025-07-23T15-54-02Z",
     "2025-02-18T16-25-55Z",
     "2025-02-07T23-21-09Z",
