9b469fa4d6292b952dc0e21d9ec26c78115328e1
replicated-ci-kurl <91491290+replicated-ci-kurl@users.noreply.github.com>
2026-02-23 22:53:46 +0000

    Automated EKCO version update 0.28.13 (#5904)
    * Create new EKCO version
    
    * Trigger Build
    
    ---------
    
    Co-authored-by: emosbaugh <371319+emosbaugh@users.noreply.github.com>
    Co-authored-by: Ethan Mosbaugh <ethan@replicated.com>

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -723,7 +723,7 @@ module.exports.InstallerVersions = {
     "0.0.1",
   ],
   ekco: [
-    "0.28.12", // cron-ekco-update
+    "0.28.13", // cron-ekco-update
   ],
   certManager: [
     // cron-cert-manager-update
