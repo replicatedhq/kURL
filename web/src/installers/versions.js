897dd122175ffa11585c2643caed529525f59208
laverya <2318911+laverya@users.noreply.github.com>
2026-03-09 01:41:08 +0000

    Create new Kubernetes version

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -24,15 +24,18 @@ module.exports.InstallerVersions = {
     "1.17.3",
     "1.16.4",
     // cron-kubernetes-update-134
+    "1.34.5",
     "1.34.4",
     "1.34.3",
     "1.34.1",
     // cron-kubernetes-update-133
+    "1.33.9",
     "1.33.8",
     "1.33.7",
     "1.33.5",
     "1.33.4",
     // cron-kubernetes-update-132
+    "1.32.13",
     "1.32.12",
     "1.32.11",
     "1.32.9",
