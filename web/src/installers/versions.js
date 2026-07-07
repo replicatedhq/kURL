f22e47c183a787a9fb4ca8fc715ff5182d25851e
replicated-ci-kurl <91491290+replicated-ci-kurl@users.noreply.github.com>
2026-07-07 00:21:28 +0000

    Automated Prometheus version update 0.92.1-87.10.1 (#6013)
    Create new Prometheus version
    
    Co-authored-by: laverya <2318911+laverya@users.noreply.github.com>

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -367,6 +367,7 @@ module.exports.InstallerVersions = {
   ],
   prometheus: [
     // cron-prometheus-update
+    "0.92.1-87.10.1",
     "0.90.1-84.5.0",
     "0.90.1-84.1.2",
     "0.89.0-82.13.2",
