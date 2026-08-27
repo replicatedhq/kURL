a3b4302ea7b16a34588f221af9e444def32af106
replicated-ci-kurl <91491290+replicated-ci-kurl@users.noreply.github.com>
2026-08-27 15:53:57 -0400

    Automated EKCO version update 0.28.15 (#6112)
    * Create new EKCO version
    
    * empty commit
    
    ---------
    
    Co-authored-by: xavpaice <5035609+xavpaice@users.noreply.github.com>
    Co-authored-by: Xav Paice <xav@replicated.com>

--- web/src/installers/versions.js
+++ web/src/installers/versions.js
@@ -741,7 +741,7 @@ module.exports.InstallerVersions = {
     "0.0.1",
   ],
   ekco: [
-    "0.28.14", // cron-ekco-update
+    "0.28.15", // cron-ekco-update
   ],
   certManager: [
     // cron-cert-manager-update
